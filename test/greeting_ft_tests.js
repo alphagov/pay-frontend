af65f7fcaebc106a60f00b8831cea6094d2de95b
Fran <fcoblaya@gmail.com>
2015-09-04 16:28:45 +0100

    Include alternative FT UI tests definitions

--- test/greeting_ft_tests.js
+++ test/greeting_ft_tests.js
@@ -1,13 +1,18 @@
-var responseTo = require(__dirname + '/utils/test_helpers.js').responseTo;
+var getTo = require(__dirname + '/utils/test_helpers.js').get;
 var app = require(__dirname + '/../server.js').getApp;
 
 describe('The /greeting endpoint returned json', function () {
 
-  it('should contain the following elements', responseTo(app, '/greeting').contains(
-    {
-      'greeting': 'Hello',
-      'name': 'World'
-    }
-  ));
+  it('should contain the following elements',
+
+    getTo(app, '/greeting')
+      .is(200)
+      .contains(
+      {
+        'greeting': 'Hello',
+        'name': 'World'
+      })
+
+  );
 
 });
--- test/utils/test_helpers.js
+++ test/utils/test_helpers.js
@@ -3,22 +3,67 @@ var should = require('chai').should();
 var cheerio = require('cheerio');
 
 module.exports = {
-  responseTo: function (app, endpoint) {
 
+  get: function (app, endpoint) {
     return {
-      contains: function(expectedResponse) {
-        return function(done) {
-          request(app)
-            .get(endpoint)
-            .set('Accept', 'application/json')
-            .expect(200)
-            .end(function(err, res) {
-              response = JSON.parse(res.text);
-              Object.keys(expectedResponse).map(function(key){
-                expectedResponse[key].should.equal(response[key]);
-              });
-              done();
-            });
+      is: function(responseCode) {
+        return {
+          contains: function(expectedResponse) {
+            return function(done) {
+              request(app)
+                .get(endpoint)
+                .set('Accept', 'application/json')
+                .expect(responseCode)
+                .end(function(err, res) {
+                  response = JSON.parse(res.text);
+                  Object.keys(expectedResponse).map(function(key){
+                    expectedResponse[key].should.equal(response[key]);
+                  });
+                  done();
+                });
+            }
+          }
+        }
+      }
+    }
+  },
+
+  post: function(app, endpoint) {
+    return {
+      withData: function(payload) {
+        return {
+          is: function(responseCode) {
+            return {
+              location: function(location) {
+                return function(done) {
+                  request(app)
+                    .post(endpoint)
+                    .set('Accept', 'application/json')
+                    .send(payload)
+                    .expect('Location', location)
+                    .expect(responseCode)
+                    .end(done);
+                }
+              },
+
+              contains: function(expectedResponse) {
+                return function(done) {
+                  request(app)
+                    .post(endpoint)
+                    .set('Accept', 'application/json')
+                    .send(payload)
+                    .expect(responseCode)
+                    .end(function(err, res) {
+                      response = JSON.parse(res.text);
+                      Object.keys(expectedResponse).map(function(key){
+                        expectedResponse[key].should.equal(response[key]);
+                      });
+                      done();
+                    });
+                }
+              }
+            }
+          }
         }
       }
     }
