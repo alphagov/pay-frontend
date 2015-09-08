31746e4d851d6c98b14a41d186d4b8c417068620
Ian Maddison <ian@i.need.stronger.coffee>
2015-09-08 18:05:30 +0100

    Remove unused docker-ci.sh

--- docker-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "x${ghprbActualCommit}" = "x" ]; then
-    COMMIT=$(git rev-parse HEAD)
-else
-    COMMIT=$ghprbActualCommit
-fi
-
-IMAGE="govukpay/frontend:$COMMIT"
-npm install && npm test && docker build -t $IMAGE . && docker push $IMAGE
