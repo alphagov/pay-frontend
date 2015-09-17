bdd4ad3703daf5cb136a93f09f6895a798ef800d
Dan Acceleanu <acceleanu@gmail.com>
2015-09-17 12:34:31 +0100

    PP-123 @acceleanu @rahul-vinayak Extract the logic for secure redirect in a separate controller.

--- app/controllers/charge_controller.js
+++ app/controllers/charge_controller.js
@@ -7,7 +7,6 @@ var response = require('../utils/response.js').response;
 var client = new Client();
 
 module.exports.bindRoutesTo = function(app) {
-  var CHARGE_PATH = '/charge';
   var CONFIRM_PATH = '/confirm';
   var CARD_DETAILS_PATH = '/card_details';
 
@@ -22,67 +21,6 @@ module.exports.bindRoutesTo = function(app) {
     'addressLine1': 'Building name/number and street',
     'addressPostcode': 'Postcode'
   };
-
-  app.get(CHARGE_PATH + '/:chargeId?', function(req, res) {
-    logger.info('GET ' + CHARGE_PATH + '/:chargeId');
-
-    var chargeTokenId = req.query.chargeTokenId;
-    logger.info('req.query.chargeTokenId=' + chargeTokenId);
-
-    var chargeId = req.params.chargeId;
-    var sessionChargeIdKey = 'ch_' + chargeId;
-
-    logger.info('req.session_state[' + sessionChargeIdKey + ']=' + req.session_state[sessionChargeIdKey])
-
-    if(!req.session_state[sessionChargeIdKey]) {
-      var connectorUrl = process.env.CONNECTOR_TOKEN_URL.replace('{chargeTokenId}', chargeTokenId);
-
-      logger.info('trying to validate token=' + chargeTokenId);
-      client.get(connectorUrl, function(tokenVerifyData, tokenVerifyResponse) {
-        logger.info('response from the connector=' + tokenVerifyResponse.statusCode);
-        if(tokenVerifyResponse.statusCode === 200) {
-          logger.info('valid token found chargeTokenId=' + chargeTokenId)
-          logger.info('tokenVerifyData=', tokenVerifyData);
-          if(chargeId != tokenVerifyData.chargeId) {
-            logger.error('Unexpected: chargeId from connector=' + tokenVerifyData.chargeId + ' != chargeId from query=' + chargeId);
-
-            renderErrorView(req,res, 'There is a problem with the payments platform');
-            return;
-          }
-
-          logger.info('trying to delete token=' + chargeTokenId);
-          client.delete(connectorUrl, function(tokenDeleteData, tokenDeleteResponse) {
-            logger.info('response from the connector=' + tokenDeleteResponse.statusCode);
-            if(tokenDeleteResponse.statusCode === 204) {
-              req.session_state[sessionChargeIdKey] = true;
-              res.redirect(303, CARD_DETAILS_PATH + '/' + chargeId);
-              return;
-            }
-            logger.error('Failed to delete token=' + chargeTokenId + ' response code from connector=' + tokenDeleteResponse.statusCode);
-            renderErrorView(req, res, 'There is a problem with the payments platform');
-          });
-          return;
-        }
-        if(tokenVerifyResponse.statusCode === 404) {
-          logger.error('Token has already been used!');
-          res.status(400).send('There is a problem with the payments platform');
-          return;
-        } else {
-          logger.error('Unexpected response code:' + connectorResponse.statusCode);
-        }
-        renderErrorView(req, res, 'There is a problem with the payments platform');
-        return;
-      }).on('error', function(err) {
-        logger.error('Exception raised calling connector');
-        renderErrorView(req, res, 'There is a problem with the payments platform');
-      });
-      return;
-    } else {
-      logger.info('token already verified chargeTokenId=' + req.query.chargeTokenId);
-    }
-
-    res.redirect(303, CARD_DETAILS_PATH + '/' + chargeId);
-  });
 
   app.get(CARD_DETAILS_PATH + '/:chargeId', function(req, res) {
     var chargeId = req.params.chargeId;
