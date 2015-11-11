09c3a10900ecb67521f0ce169251cc81aca452bd
Ian Maddison <ian@i.need.stronger.coffee>
2015-11-11 11:14:50 +0000

    New run-here.sh that replaces the running docker component with run from source. Dockerfile tweaks to support this way of working. Remove old dev support.

---
+++ run-here.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ENV_FILE="$WORKSPACE/pay-scripts/services/connector.env"
+if [ -f $ENV_FILE ]
+then
+  set -a
+  source $ENV_FILE
+  set +a  
+fi
+
+# redirect dockerhost:$PORT to here
+docker run -d --name pay-frontend-redirector --net host \
+ -e IN_PORT=$PORT -e OUT_PORT=$PORT -e OUT_IP=192.168.99.1 govukpay/devhelper
+
+npm run start
