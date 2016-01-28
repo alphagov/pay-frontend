23dd7a3d4e755ee39dac3cd7077a89071bdaf55b
kakumara <kasun.kumara@gmail.com>
2016-01-28 12:19:15 +0000

    PP-429 enable google analytics
      - Enabling google analytics using the direct apporach.
      - Changed the global header so that all pages in Frontend will contain google analytics.
      - Keeping analytics script seperate in case if we need to include this selectively
      - Testing all UI's, if they include the google analytics script.

---
+++ test/analytics_ui_tests.js
@@ -0,0 +1,45 @@
+var renderTemplate = require(__dirname + '/utils/html_assertions.js').render;
+var should = require('chai').should();
+
+describe('Frontend analytics', function () {
+
+    var googleAnalyticsScript = '//www.google-analytics.com/analytics.js';
+
+    it('should be enabled in charge view', function () {
+        var templateData = {
+            'amount': '50.00'
+        };
+        var body = renderTemplate('charge', templateData);
+        body.should.containSelector('script').withText(googleAnalyticsScript);
+    });
+
+    it('should be enabled in confirm view', function () {
+        var templateData = {
+            'cardNumber': "************5100",
+            'expiryDate': "11/99",
+            'amount': "10.00",
+            'paymentDescription': "Payment Description",
+            'cardholderName': 'Francisco Blaya-Gonzalvez',
+            'address': '1 street lane, avenue city, AB1 3DF',
+            'serviceName': 'Service 1'
+        };
+
+        var body = renderTemplate('confirm', templateData);
+        body.should.containSelector('script').withText(googleAnalyticsScript);
+    });
+
+    it('should be enabled in error view', function() {
+        var msg = 'shut up and take my money!';
+        var body = renderTemplate('error', { 'message' : msg });
+        body.should.containSelector('script').withText(googleAnalyticsScript);
+    });
+
+    it('should be enabled in error with return url view', function() {
+        var msg = 'shut up and take my money!';
+        var return_url = 'http://some.return.url';
+        var body = renderTemplate('error',
+            { 'message' : msg,
+              'return_url': return_url});
+        body.should.containSelector('script').withText(googleAnalyticsScript);
+    });
+});
