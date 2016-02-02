cf951fa00585e4d7d8249e92bafd59dbbecd1e2d
kakumara <kasun.kumara@gmail.com>
2016-02-02 10:52:51 +0000

    pp-429 fix review comments
      - removed (possibly) invalid space
      - safer check for environment variable

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_too
 app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico')));
 app.use(function (req, res, next) {
   res.locals.assetPath = '/public/';
-  if(process.env.ANALYTICS_TRACKING_ID === undefined) {
+  if(typeof process.env.ANALYTICS_TRACKING_ID === "undefined") {
     logger.warn('Google Analytics Tracking ID [ANALYTICS_TRACKING_ID] is not set');
     res.locals.analyticsTrackingId = ""; //to not break the app
   } else {
