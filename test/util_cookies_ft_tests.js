da09792b733a5d64d113282c0e7f8ad83a43350f
Kasun Kumara <kasun.kumara@gmail.com>
2016-02-12 11:17:57 +0000

    PP-487 putting the env variable based cookie securing back
     - This is required because, node cookies library 'lib/cookies.js' doesn't spit out cookis based on proxy headers, rather they make it secured always if 'secureProxy: true' option is present.
     - so we need to manually switch it to false on dev environments

--- test/util_cookies_ft_tests.js
+++ test/util_cookies_ft_tests.js
@@ -4,8 +4,16 @@ var cookies  = require(__dirname + '/../app/utils/cookies.js');
 
 describe('frontend cookie', function () {
 
-  it('should have the correct structure', function () {
+  it('should have secure proxy off in unsecured environment', function () {
+    process.env.SECURE_COOKIE_OFF = "true";
     assert.equal('frontend_state', cookies.fronendCookie().cookieName);
+    assert.equal(true, cookies.fronendCookie().proxy);
+    assert.deepEqual({httpOnly: true, secureProxy: false}, cookies.fronendCookie().cookie);
+  });
+
+  it('should have secure proxy on in a secured https environment', function () {
+    process.env.SECURE_COOKIE_OFF="false";
+    assert.equal('frontend_state',cookies.fronendCookie().cookieName);
     assert.equal(true, cookies.fronendCookie().proxy);
     assert.deepEqual({httpOnly: true, secureProxy: true}, cookies.fronendCookie().cookie);
   });
