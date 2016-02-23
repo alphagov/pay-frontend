d79d2dd6b6335b0e7602e323e769cd32b9161779
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-02-23 10:42:03 +0000

    PP-219 remove portfinder weirdness
    this code was there due to a misunderstanding how nock worked, is not needed

--- test/enforce_status_views_tests.js
+++ test/enforce_status_views_tests.js
@@ -11,62 +11,56 @@ var nock = require('nock');
 
 var cookie = require(__dirname + '/test_helpers/session.js');
 
-var get_charge_request = require(__dirname + '/test_helpers/test_helpers.js').get_charge_request;
-var default_connector_response_for_get_charge = require(__dirname + '/test_helpers/test_helpers.js').default_connector_response_for_get_charge;
-var connector_response_for_put_charge = require(__dirname + '/test_helpers/test_helpers.js').connector_response_for_put_charge;
+var chargeId = '23144323';
+var frontendCardDetailsPath = '/card_details';
 
-portfinder.getPort(function (err, connectorPort) {
-  var chargeId = '23144323';
-  var frontendCardDetailsPath = '/card_details';
+describe('The /confirm endpoint undealt with states', function () {
+  var confirm_not_allowed_statuses = [
+    'AUTHORISATION SUBMITTED',
+    'CREATED',
+    'AUTHORISATION REJECTED',
+    'READY_FOR_CAPTURE',
+    'SYSTEM ERROR',
+    'SYSTEM CANCELLED',
+  ];
+  beforeEach(function() {
+    nock.cleanAll();
+    process.env.CONNECTOR_HOST = "http://aserver:9000";
+  });
 
-  describe('The /confirm endpoint undealt with states', function () {
-    var confirm_not_allowed_statuses = [
-      'AUTHORISATION SUBMITTED',
-      'CREATED',
-      'AUTHORISATION REJECTED',
-      'READY_FOR_CAPTURE',
-      'SYSTEM ERROR',
-      'SYSTEM CANCELLED',
-    ];
-    beforeEach(function() {
-      nock.cleanAll();
-      process.env.CONNECTOR_HOST = "http://aserver:9000";
-    });
-
-    afterEach(function() {
-      nock.cleanAll();
-      process.env.CONNECTOR_HOST = undefined;
-    });
+  afterEach(function() {
+    nock.cleanAll();
+    process.env.CONNECTOR_HOST = undefined;
+  });
 
 
 
 
-    confirm_not_allowed_statuses.forEach(function (status) {
-      var fullSessionData = {
-        'amount': 1000,
-        'paymentDescription': 'Test Description',
-        'cardNumber': "************5100",
-        'expiryDate': "11/99",
-        'cardholderName': 'T Eulenspiegel',
-        'address': 'Kneitlingen, Brunswick, Germany',
-        'serviceName': 'Pranks incorporated'
-      };
+  confirm_not_allowed_statuses.forEach(function (status) {
+    var fullSessionData = {
+      'amount': 1000,
+      'paymentDescription': 'Test Description',
+      'cardNumber': "************5100",
+      'expiryDate': "11/99",
+      'cardholderName': 'T Eulenspiegel',
+      'address': 'Kneitlingen, Brunswick, Germany',
+      'serviceName': 'Pranks incorporated'
+    };
 
-      it('should error when the payment status is ' + status, function (done) {
-        nock(process.env.CONNECTOR_HOST)
-        .get('/v1/frontend/charges/' + chargeId).reply(200,helper.raw_successful_get_charge(
-          status,"http://www.example.com/service"
-        ));
+    it('should error when the payment status is ' + status, function (done) {
+      nock(process.env.CONNECTOR_HOST)
+      .get('/v1/frontend/charges/' + chargeId).reply(200,helper.raw_successful_get_charge(
+        status,"http://www.example.com/service"
+      ));
 
-        request(app)
-          .get(frontendCardDetailsPath + '/' + chargeId + '/confirm')
-          .set('Cookie', ['frontend_state=' + cookie.create(chargeId, fullSessionData)])
-          .expect(function(res){
+      request(app)
+        .get(frontendCardDetailsPath + '/' + chargeId + '/confirm')
+        .set('Cookie', ['frontend_state=' + cookie.create(chargeId, fullSessionData)])
+        .expect(function(res){
 
-            helper.expectTemplateTohave(res,"message", "View " +  status.toUpperCase().replace(" ", "_") + " not found");
-          })
-          .end(done);
-      });
+          helper.expectTemplateTohave(res,"message", "View " +  status.toUpperCase().replace(" ", "_") + " not found");
+        })
+        .end(done);
     });
   });
 });
