d17545db719c4df6ed62235a77f73232a6b37540
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-03-15 12:53:38 +0000

    PP-441 Error ui tests
    accidentally removed this

--- test/error_ui_tests.js
+++ test/error_ui_tests.js
@@ -1,6 +1,11 @@
 
-describe('The error view', function() {
+var renderTemplate = require(__dirname + '/test_helpers/html_assertions.js').render;
+
+ describe('The error view', function() {
   it('should render an error message', function() {
+    var msg = 'shut up and take my money!';
+    var body = renderTemplate('error', { 'message' : msg });
+    body.should.containSelector('#errorMsg').withText(msg);
 
   });
 });
