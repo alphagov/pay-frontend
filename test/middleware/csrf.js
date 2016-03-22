5324866cf34b007f79d43a00f8f7acd081300948
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-03-22 16:18:44 +0000

    PP-502 write tests for csrf module

---
+++ test/middleware/csrf.js
@@ -0,0 +1,138 @@
+var should = require('chai').should();
+var assert = require('assert');
+var sinon  = require('sinon');
+var _      = require('lodash');
+var expect = require('chai').expect;
+var nock   = require('nock');
+var paths  = require('../../app/paths.js');
+var helper = require(__dirname + '/../test_helpers/test_helpers.js');
+
+var csrf = require(__dirname + '/../../app/middleware/csrf.js');
+
+
+describe('retrieve param test', function () {
+
+  var response = {
+    status: function(){},
+    render: function(){},
+    locals: {}
+  },
+  status = undefined,
+  render = undefined,
+  next   = undefined,
+  validGetRequest =  {
+    params: { chargeId: "foo" },
+    body: {},
+    route: { methods: { get: true}},
+    frontend_state: {
+      ch_foo: {
+        csrfSecret: process.env.CSRF_USER_SECRET
+      },
+    }
+  };
+
+  var noSession = _.cloneDeep(validGetRequest);
+  delete noSession.frontend_state.ch_foo;
+
+  var noSecret = _.cloneDeep(validGetRequest);
+  delete noSecret.frontend_state.ch_foo.csrfSecret;
+
+  var invalidPost = _.cloneDeep(validGetRequest);
+  delete invalidPost.route.methods.get;
+  var invalidPut = _.cloneDeep(invalidPost);
+
+  invalidPost.route.methods.post  = true;
+  invalidPut.route.methods.put    = true;
+
+  var validPost = _.cloneDeep(invalidPost);
+  validPost.body.csrfToken = helper.csrfToken();
+
+  var validPut = _.cloneDeep(invalidPut);
+  validPut.body.csrfToken = helper.csrfToken();
+
+  var assertErrorRequest = function(next,resp,status,render) {
+    expect(next.called).to.not.be.true;
+    expect(resp.locals.csrf).to.be.undefined;
+    assert(status.calledWith(404));
+    assert(render.calledWith('error',
+      { message: "Page cannot be found",
+        viewName: "NOT_FOUND" }
+      ));
+  },
+
+  assertValidRequest = function(next,resp,status,render) {
+    expect(next.called).to.be.true;
+    expect(resp.locals.csrf).to.not.be.undefined;
+  };
+
+
+
+  beforeEach(function(){
+    status = sinon.stub(response,"status");
+    render = sinon.stub(response,"render");
+    next   = sinon.spy();
+    nock.cleanAll();
+  });
+
+  afterEach(function(){
+    status.restore();
+    render.restore();
+  });
+
+  it('should append csrf on get request', function () {
+    var resp = _.cloneDeep(response);
+    csrf(validGetRequest,resp,next);
+    assertValidRequest(next,resp,status,render);
+  });
+
+  it('should error if no charge in session', function () {
+    var resp = _.cloneDeep(response);
+    csrf(noSession,resp,next);
+    assertErrorRequest(next,resp,status,render);
+
+  });
+
+  it('should error if no secret in session', function () {
+    var resp = _.cloneDeep(response);
+    csrf(noSecret,resp,next);
+    assertErrorRequest(next,resp,status,render);
+  });
+
+  it('should be successful on post if valid post and append token to used tokens', function () {
+    var resp = _.cloneDeep(response);
+    csrf(validPost,resp,next);
+    assertValidRequest(next,resp,status,render);
+    assert.equal(validPost.frontend_state.ch_foo.csrfTokens[0],validPost.body.csrfToken);
+  });
+
+  it('should be unsuccessful on post if token is already used', function () {
+    var resp = _.cloneDeep(response);
+    csrf(validPost,resp,next);
+    assertErrorRequest(next,resp,status,render);
+  });
+
+  it('should error if no csrfToken in post request', function () {
+    var resp = _.cloneDeep(response);
+    csrf(invalidPost,resp,next);
+    assertErrorRequest(next,resp,status,render);
+  });
+
+  it('should be successful on post if valid put', function () {
+    var resp = _.cloneDeep(response);
+    csrf(validPut,resp,next);
+    assertValidRequest(next,resp,status,render);
+  });
+
+  it('should be unsuccessful on put if token is already used', function () {
+    var resp = _.cloneDeep(response);
+    csrf(validPut,resp,next);
+    assertErrorRequest(next,resp,status,render);
+  });
+
+  it('should error if no csrfToken in post request', function () {
+    var resp = _.cloneDeep(response);
+    csrf(invalidPut,resp,next);
+    assertErrorRequest(next,resp,status,render);
+  });
+
+});
