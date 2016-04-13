9dd3af5b635cfc78904b047d1c56db3a3371bb94
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-04-13 15:46:30 +0100

    PP-499 add in label changing on unsupported card

--- app/assets/javascripts/form-card-type.js
+++ app/assets/javascripts/form-card-type.js
@@ -2,17 +2,22 @@ var showCardType = function(){
   var form          = $('form#card-details'),
   acceptedCards     = form.find('.accepted-cards'),
   cardInput         = form.find('input#card-no'),
-  cards             = form.find('.accepted-cards > li');
+  cards             = form.find('.accepted-cards > li'),
+  cardNoLabel       = form.find('.card-no-label'),
+  cardNoFormGroup   = form.find('.card-no-group'),
+  cardTypes         = cards.map(function(){ return $(this).attr('data-key'); });
+  notSupportedString= i18n.chargeController.fieldErrors.fields.cardNo.card_not_supported
 
   var init = function(){
     cardInput.on('keyup',showCardType);
   },
 
   showCardType = function(){
-    var cardType = module.creditCardType($(this).val().replace(/\D/g,''));
+    var number = $(this).val().replace(/\D/g,'');
+    var cardType = module.creditCardType(number);
     unSelectAll();
     checkEmpty();
-    console.log(getCardTypes());
+    checkAllowed(cardType);
     if (cardType.length !== 1) return;
     selectCard(cardType[0].type);
   },
@@ -27,7 +32,21 @@ var showCardType = function(){
 
   checkEmpty = function(){
     acceptedCards.toggleClass('field-empty',cardInput.val().length === 0);
-  }
+  },
+
+  checkAllowed = function(cardType){
+    if (cardType.length !== 1) return replaceCardLabelWithOriginal();
+    var supported = $.inArray(cardType[0].type,cardTypes) !== -1;
+
+    if (supported) return replaceCardLabelWithOriginal();
+    cardNoFormGroup.addClass('error');
+    cardNoLabel.text(notSupportedString);
+  },
+
+  replaceCardLabelWithOriginal = function(){
+    cardNoFormGroup.removeClass('error');
+    cardNoLabel.text(cardNoLabel.attr('data-original-label'));
+  };
 
 
   init();
