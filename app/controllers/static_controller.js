233ceea4606c81c9c8b967d01f3994137ac02c9a
Raul Garcia <raul.garcia@digital.cabinet-office.gov.uk>
2016-07-11 14:48:35 +0100

    PP-800 Log naxsi error

--- app/controllers/static_controller.js
+++ app/controllers/static_controller.js
@@ -1,6 +1,7 @@
 /*jslint node: true */
 "use strict";
 var views = require('../utils/views.js');
+var logger= require('winston');
 
 module.exports = {
   privacy: function(req,res){
@@ -8,6 +9,7 @@ module.exports = {
   },
 
   naxsi_error: function(req,res){
+    logger.info('NAXSI ERROR:- ' + req.headers["x-naxsi_sig"]);
     var _views = views.create();
     _views.display(res, "NAXSI_SYSTEM_ERROR");
   }
