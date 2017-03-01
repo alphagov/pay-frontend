9b6811cb85354c06935b7b67dd9b81f5f8e68065
Silvia Mandala <silvia.mandala@digital.cabinet-office.gov.uk>
2017-03-01 15:01:41 +0000

    PP-1753 Add tests for the return controller
    - Having a duplicate of the connector's logic (ie. the
    `CANCELABLE_STATES` structure) in frontend is not ideal, as
      any change in connector can potentially put the controller out
      of sync, returning spurious results.
    - A major refactor is out of the scope of this fix, but we should
      keep it in mind nevertheless

--- app/controllers/return_controller.js
+++ app/controllers/return_controller.js
@@ -2,30 +2,32 @@ var Charge = require('../models/charge.js'),
   StateModel = require('../models/state.js'),
   views = require('../utils/views.js'),
   CORRELATION_HEADER = require('../utils/correlation_header.js').CORRELATION_HEADER,
-    withAnalyticsError = require('../utils/analytics.js').withAnalyticsError;
+  withAnalyticsError = require('../utils/analytics.js').withAnalyticsError;
 
 var logger = require('winston');
+
+let CANCELABLE_STATES = [
+  StateModel.CREATED,
+  StateModel.ENTERING_CARD_DETAILS,
+  StateModel.AUTH_SUCCESS,
+  StateModel.AUTH_READY,
+  StateModel.CAPTURE_READY,
+  StateModel.AUTH_3DS_REQUIRED,
+  StateModel.AUTH_3DS_READY
+];
 
 module.exports.return = function (req, res) {
   'use strict';
 
-  var correlationId = req.headers[CORRELATION_HEADER] || '',
+  let correlationId = req.headers[CORRELATION_HEADER] || '',
    _views = views.create({}),
   doRedirect = () => res.redirect(req.chargeData.return_url),
-  chargeModel = Charge(correlationId),
-  
-  cancelStates = [
-    StateModel.CREATED, 
-    StateModel.ENTERING_CARD_DETAILS,
-    StateModel.AUTH_SUCCESS,
-    StateModel.AUTH_READY,
-    StateModel.CAPTURE_READY,
-    StateModel.AUTH_3DS_REQUIRED,
-    StateModel.AUTH_3DS_READY
-  ];
+  chargeModel = Charge(correlationId);
 
-  if (cancelStates.indexOf(req.chargeData.status) === -1) return doRedirect();
-  
-  logger.warn('Return controller cancelling payment', {'chargeId': req.chargeId});
-  chargeModel.cancel(req.chargeId).then(doRedirect, ()=> _views.display(res, 'SYSTEM_ERROR', withAnalyticsError()));
+  if (CANCELABLE_STATES.includes(req.chargeData.status)) {
+    chargeModel.cancel(req.chargeId).then(
+      () => logger.warn('Return controller cancelled payment', {'chargeId': req.chargeId}),
+      () => _views.display(res, 'SYSTEM_ERROR', withAnalyticsError()));
+  }
+    doRedirect();
 };
