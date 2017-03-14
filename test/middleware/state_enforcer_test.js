86a15894cf0d1866e166d697fd1d3f583f39e8e7
Silvia Mandala <silvia.mandala@digital.cabinet-office.gov.uk>
2017-03-14 12:18:47 +0000

    PP-1365 Add descriptive google analytics to the payment journey
    - We are currently sending a pageview on google analytics purely
      depending on the endpoint in frontend. This is not the actual
      truth as frontend displays multiple views under the same endpoint
    - As an example, think about a user pressing the back button from
      the confirm page. The endpoint stays `/confirm`, but the user
      sees the "Your payment is in progress" page.
    - Analytics are associated to actual views; in the current frontend's
      logic, the state enforcer is also doing routing as part of its duties.
      This means we have two different points where we decide which view to
      render: the charge_controller and the state_enforcer.
    - There are now analytics "virtual endpoints" associated to views. The state
      enforcer will pick them up and append them to the base path, which is
      `/card_details/:charge_id`. I know that in some way this is enhancing
      the state enforcer's magic powers, but its refactoring would entail a
      refactor of the whole frontend. I still think it's something that
      should be done, at some point.
    - So, going back to the previous example, the user is on the confirm
      page and presses the back button. The charge is in `AUTHORISATION_SUCCESS`
      and the state enforcer kicks in, rendering automagically the `auth_success` page.
      The analytics for that view will be `/in-progress`, even though the
      endpoint is still `/confirm`.
    - The charge controller now always goes through `views.display` instead
      of calling res.render directly.
      The path of the analytics' path is constructed appeding the
    `analyticsPage` in `views.js` to the base url, which is `card.new`.

--- test/middleware/state_enforcer_test.js
+++ test/middleware/state_enforcer_test.js
@@ -55,7 +55,7 @@ describe('state enforcer', function () {
       actionName: "card.new",
       chargeData: { status: 'AUTHORISATION_SUCCESS', return_url: "foo" , gateway_account: {analytics_id: 'Test AnalyticsID', type: 'Test Type', payment_provider: 'Test Provider'}},
       chargeId: 1
-    },response,next)
+    },response,next);
     expect(next.notCalled).to.be.true;
     assert(status.calledWith(200));
     assert(render.calledWith("errors/incorrect_state/auth_success",
@@ -63,7 +63,8 @@ describe('state enforcer', function () {
         analytics: {
           analyticsId: "Test AnalyticsID",
           type: "Test Type",
-          paymentProvider: "Test Provider"
+          paymentProvider: "Test Provider",
+          path: '/card_details/1/in-progress'
         },
         returnUrl: '/return/1',
         viewName: 'AUTHORISATION_SUCCESS' }
@@ -79,6 +80,69 @@ describe('state enforcer', function () {
     },response,next)}).to.throw(/Cannot find correct states for action/);
   });
 
+  it('should render the confirm view when coming back from the service with the correct analytics', function () {
+    stateEnforcer({
+      actionName: "card.confirm",
+      chargeData: { status: 'CAPTURED', return_url: "foo" , gateway_account: {analytics_id: 'Test AnalyticsID', type: 'Test Type', payment_provider: 'Test Provider'}},
+      chargeId: 1
+    },response,next);
+    expect(next.notCalled).to.be.true;
+    assert(status.calledWith(200));
+    assert(render.calledWith("errors/charge_confirm_state_completed",
+      { status: 'successful',
+        chargeId: 1,
+        analytics: {
+          analyticsId: "Test AnalyticsID",
+          type: "Test Type",
+          paymentProvider: "Test Provider",
+          path: '/card_details/1/success-return'
+        },
+        returnUrl: '/return/1',
+        viewName: 'CAPTURED' }
+    ));
+  });
+
+  it('should render the auth_failure view the correct analytics when auth is rejected', function () {
+    stateEnforcer({
+      actionName: "card.new",
+      chargeData: { status: 'AUTHORISATION_REJECTED', return_url: "foo" , gateway_account: {analytics_id: 'Test AnalyticsID', type: 'Test Type', payment_provider: 'Test Provider'}},
+      chargeId: 1
+    },response,next);
+    expect(next.notCalled).to.be.true;
+    assert(status.calledWith(200));
+    assert(render.calledWith("errors/incorrect_state/auth_failure",
+      { chargeId: 1,
+        analytics: {
+          analyticsId: "Test AnalyticsID",
+          type: "Test Type",
+          paymentProvider: "Test Provider",
+          path: '/card_details/1/problem'
+        },
+        returnUrl: '/return/1',
+        viewName: 'AUTHORISATION_REJECTED' }
+    ));
+  });
+
+  it('should render the auth_failure view the correct analytics when auth is cancelled', function () {
+    stateEnforcer({
+      actionName: "card.new",
+      chargeData: { status: 'AUTHORISATION_CANCELLED', return_url: "foo" , gateway_account: {analytics_id: 'Test AnalyticsID', type: 'Test Type', payment_provider: 'Test Provider'}},
+      chargeId: 1
+    },response,next);
+    expect(next.notCalled).to.be.true;
+    assert(status.calledWith(200));
+    assert(render.calledWith("errors/incorrect_state/auth_failure",
+      { chargeId: 1,
+        analytics: {
+          analyticsId: "Test AnalyticsID",
+          type: "Test Type",
+          paymentProvider: "Test Provider",
+          path: '/card_details/1/problem'
+        },
+        returnUrl: '/return/1',
+        viewName: 'AUTHORISATION_CANCELLED' }
+    ));
+  });
 
 
 });
