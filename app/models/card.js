f6cef32dd33a6f6be6f1769d27f86c14be6ca712
Will Pearson <will.pearson@digital.cabinet-office.gov.uk>
2017-06-02 11:07:20 +0100

    PP-2152: Make cardid error more obvious
    Because CARD SERVICE is a potentially a little too
    overloaded.

--- app/models/card.js
+++ app/models/card.js
@@ -43,7 +43,7 @@ var checkCard = function(cardNo,allowed, correlationId) {
       }
       return defer.resolve(cardBrand);
     }).on('error',function(error){
-      logger.error(`[${correlationId}] ERROR CALLING CARD SERVICE`, error);
+      logger.error(`[${correlationId}] ERROR CALLING CARDID AT ${cardIdClient.CARD_URL}`, error);
       logger.info(`[${correlationId}] - %s to %s ended - total time %dms`, 'POST', cardIdClient.cardUrl, new Date() - startTime);
       defer.resolve();
     });
