a9925847712df68cc026cbef7520c33fcb95e8a9
Kasun Kumara <kasun.kumara@digital.cabinet-office.gov.uk>
2017-08-17 09:16:45 +0100

    PP-2502 Reverting the custom branding bits until Jon's back
    It seems the changes to custom branding does not defaults to default GOV.UK Pay template if `custom_branding` does not exist (could not see the GOV.UK header test). Not sure how this could fixed for now.
    
    Reverting the changes until @jonheslop is back
    
    NOTE: Uncomment the commented lines in `charge_ui_tests.js` after fixing this. These tests makes sure the custom branding displays if exists.

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
       },
       {
         expand: true,
-        cwd: 'node_modules/govuk_template_mustache_inheritance/',
+        cwd: 'node_modules/govuk_template_mustache/',
         src: '**',
         dest: 'govuk_modules/govuk_template/'
       }]
@@ -67,8 +67,7 @@ module.exports = function (grunt) {
       files: {
         'public/stylesheets/application.min.css': [
           'public/stylesheets/application.css', 'govuk_modules/accessible-autocomplete/accessible-autocomplete.min.css'
-        ],
-        'public/stylesheets/custom.min.css': 'public/stylesheets/custom.css'
+        ]
       }
     }
   }
@@ -245,9 +244,21 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks(task)
   })
 
+  grunt.registerTask(
+    'convert_template',
+    'Converts the govuk_template to use mustache inheritance',
+    function () {
+      var script = require(path.join(__dirname, '/lib/template-conversion.js'))
+
+      script.convert()
+      grunt.log.writeln('govuk_template converted')
+    }
+  )
+
   grunt.registerTask('generate-assets', [
     'clean',
     'copy',
+    'convert_template',
     'replace',
     'sass',
     'browserify',
