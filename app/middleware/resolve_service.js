03a9cf13083adccdc21cd9762da000e35a7c1170
maxcbc <maxcbc@live.com>
2018-02-03 17:03:02 +0000

    PP-3266 Refactored resolve_service middleware - removed unnecessary variables in resolve_service middleware - implemented 'prefer-const' style for variable declarations

--- app/middleware/resolve_service.js
+++ app/middleware/resolve_service.js
@@ -1,21 +1,20 @@
 'use strict'
 
+// npm dependencies
 const logger = require('winston')
 
+// local dependencies
 const views = require('../utils/views.js')
 const CORRELATION_HEADER = require('../utils/correlation_header.js').CORRELATION_HEADER
 const withAnalyticsError = require('../utils/analytics.js').withAnalyticsError
 const getAdminUsersClient = require('../services/clients/adminusers_client')
 
-module.exports = function (req, res, next) {
-  'use strict'
+module.exports = (req, res, next) => {
   const _views = views.create()
-
   if (!req.chargeId && !req.chargeData) {
     _views.display(res, 'UNAUTHORISED', withAnalyticsError())
   } else {
-    let correlationId = req.headers[CORRELATION_HEADER]
-    return getAdminUsersClient({correlationId: correlationId})
+    return getAdminUsersClient({correlationId: req.headers[CORRELATION_HEADER]})
       .findServiceBy({gatewayAccountId: req.chargeData.gateway_account.gateway_account_id})
       .then(service => {
         res.locals.service = service
