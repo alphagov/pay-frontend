8d2b49635d99941d715b1572800858b1847a2058
maxcbc <maxcbc@live.com>
2018-02-03 21:41:27 +0000

    PP-3266 Refactored logging util - switched to strict mode - prefer const - switched to exports over module.exports

--- app/utils/logging.js
+++ app/utils/logging.js
@@ -1,36 +1,37 @@
-/* jslint node: true */
 'use strict'
-var logger = require('winston')
-module.exports = {
 
-  authChargePost: function (url) {
-    logger.debug('Calling connector to authorize a charge (post card details) -', {
-      service: 'connector',
-      method: 'POST',
-      url: url
-    })
-  },
-  failedChargePost: function (status, url) {
-    logger.warn('Calling connector to authorize a charge (post card details) failed -', {
-      service: 'connector',
-      method: 'POST',
-      status: status,
-      url: url
-    })
-  },
+// npm dependencies
+const logger = require('winston')
 
-  failedChargePostException: function (err) {
-    logger.error('Calling connector to authorize a charge (post card details) threw exception -', {
-      service: 'connector',
-      method: 'POST',
-      error: err
-    })
-  },
-  failedChargePatch: function (err) {
-    logger.warn('Calling connector to patch a charge failed -', {
-      service: 'connector',
-      method: 'POST',
-      err: err
-    })
-  }
+exports.authChargePost = url => {
+  logger.debug('Calling connector to authorize a charge (post card details) -', {
+    service: 'connector',
+    method: 'POST',
+    url: url
+  })
+}
+
+exports.failedChargePost = (status, url) => {
+  logger.warn('Calling connector to authorize a charge (post card details) failed -', {
+    service: 'connector',
+    method: 'POST',
+    status: status,
+    url: url
+  })
+}
+
+exports.failedChargePostException = err => {
+  logger.error('Calling connector to authorize a charge (post card details) threw exception -', {
+    service: 'connector',
+    method: 'POST',
+    error: err
+  })
+}
+
+exports.failedChargePatch = err => {
+  logger.warn('Calling connector to patch a charge failed -', {
+    service: 'connector',
+    method: 'POST',
+    err: err
+  })
 }
