e1e5267a1430840fbfa34685bb1300f1d55d8c7b
maxcbc <maxcbc@live.com>
2018-02-03 21:38:52 +0000

    PP-3266 Refactored luhn util - switched to strict mode - prefer const - got rid of unnecessary IIFE - switched to exports over module.exports

--- app/utils/luhn.js
+++ app/utils/luhn.js
@@ -1,47 +1,36 @@
+'use strict'
 // COPIED OVER FROM NODE_LUHN
 // https://github.com/JamesEggers1/node-luhn/issues/12
-'use strict'
-module.exports = (function () {
-  function validate (cardNumber) {
-    var trimmed = String(cardNumber).replace(/[\s]/g, '')
-    var length = trimmed.length
-    var odd = false
-    var total = 0
-    var calc
-    var calc2
+exports.validate = cardNumber => {
+  const trimmed = String(cardNumber).replace(/[\s]/g, '')
+  const length = trimmed.length
+  let odd = false
+  let total = 0
+  let calc
+  let calc2
 
-    if (length === 0) {
-      return true
-    }
+  if (length === 0) return true
+  if (!/^[0-9]+$/.test(trimmed)) return false
 
-    if (!/^[0-9]+$/.test(trimmed)) {
-      return false
-    }
+  for (let i = length; i > 0; i--) {
+    calc = parseInt(trimmed.charAt(i - 1))
+    if (!odd) {
+      total += calc
+    } else {
+      calc2 = calc * 2
 
-    for (var i = length; i > 0; i--) {
-      calc = parseInt(trimmed.charAt(i - 1))
-      if (!odd) {
-        total += calc
-      } else {
-        calc2 = calc * 2
-
-        switch (calc2) {
-          case 10: calc2 = 1; break
-          case 12: calc2 = 3; break
-          case 14: calc2 = 5; break
-          case 16: calc2 = 7; break
-          case 18: calc2 = 9; break
-          default: calc2 = calc2 // eslint-disable-line
-        }
-        total += calc2
+      switch (calc2) {
+        case 10: calc2 = 1; break
+        case 12: calc2 = 3; break
+        case 14: calc2 = 5; break
+        case 16: calc2 = 7; break
+        case 18: calc2 = 9; break
+        default: break
       }
-      odd = !odd
+      total += calc2
     }
-
-    return ((total % 10) === 0)
+    odd = !odd
   }
 
-  return {
-    validate: validate
-  }
-}())
+  return (total % 10) === 0
+}
