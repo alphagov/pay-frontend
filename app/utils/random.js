fa01fc9f844ae9818d80e389577cda3e48faab21
maxcbc <maxcbc@live.com>
2018-02-03 21:27:50 +0000

    PP-3266 Refactored random util - implemented 'prefer-const' style for variable declarations - switched to exports over module.exports for clarity

--- app/utils/random.js
+++ app/utils/random.js
@@ -1,33 +1,21 @@
 'use strict'
 
-function randomInt (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+exports.key = length => {
+  const buf = []
+  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < length; ++i) {
+    buf.push(chars[exports.randomInt(0, chars.length - 1)])
+  }
 
-function randomUuid () {
+  return buf.join('')
+}
+exports.randomUuid = () => {
   // See:
   // http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-  return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, c => {
     const r = Math.random() * 16 | 0
     const v = c === 'x' ? r : (r & 0x3 | 0x8)
     return v.toString(16)
   })
 }
-
-function key (length) {
-  let buf = []
-  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
-  const charlen = chars.length
-
-  for (let i = 0; i < length; ++i) {
-    buf.push(chars[randomInt(0, charlen - 1)])
-  }
-
-  return buf.join('')
-}
-
-module.exports = {
-  randomInt: randomInt,
-  randomUuid: randomUuid,
-  key: key
-}
+exports.randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
