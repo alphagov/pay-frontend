d8a135b2d4881f67777b5b6dea04acf28a5f4621
maxcbc <maxcbc@live.com>
2018-02-04 13:16:47 +0000

    PP-3266 Refactored email_tools util - switched to module level strict mode - prefer const - removed unnecessary inner variable declarations - removed unnecessary inner functions

--- app/utils/email_tools.js
+++ app/utils/email_tools.js
@@ -1,40 +1,27 @@
+'use strict'
+
 /* Uses Regexes from the rfc822-validate library.
    We use rfc822-validate in our email validation
    so this follows that standard. */
 
-var emailTools = function (email) {
-  'use strict'
+const sQtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
+const sDtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
+const sAtom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
+const sQuotedPair = '\\x5c[\\x00-\\x7f]'
+const sDomainLiteral = '\\x5b(' + sDtext + '|' + sQuotedPair + ')*\\x5d'
+const sQuotedString = '\\x22(' + sQtext + '|' + sQuotedPair + ')*\\x22'
+const sSubDomain = '(' + sAtom + '|' + sDomainLiteral + ')'
+const sWord = '(' + sAtom + '|' + sQuotedString + ')'
+const sDomain = sSubDomain + '(\\x2e' + sSubDomain + ')*'
+const sLocalPart = sWord + '(\\x2e' + sWord + ')*'
+const sAddrSpec = '(' + sLocalPart + ')' + '\\x40' + '(' + sDomain + ')' // complete RFC822 email address spec
+const sValidEmail = '^' + sAddrSpec + '$' // as whole string
+const reValidEmail = new RegExp(sValidEmail)
 
-  var init = (function () {
-    var sQtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
-    var sDtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
-    var sAtom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
-    var sQuotedPair = '\\x5c[\\x00-\\x7f]'
-    var sDomainLiteral = '\\x5b(' + sDtext + '|' + sQuotedPair + ')*\\x5d'
-    var sQuotedString = '\\x22(' + sQtext + '|' + sQuotedPair + ')*\\x22'
-    var sDomainRef = sAtom
-    var sSubDomain = '(' + sDomainRef + '|' + sDomainLiteral + ')'
-    var sWord = '(' + sAtom + '|' + sQuotedString + ')'
-    var sDomain = sSubDomain + '(\\x2e' + sSubDomain + ')*'
-    var sLocalPart = sWord + '(\\x2e' + sWord + ')*'
-    var sAddrSpec = '(' + sLocalPart + ')' + '\\x40' + '(' + sDomain + ')' // complete RFC822 email address spec
-    var sValidEmail = '^' + sAddrSpec + '$' // as whole string
-
-    var reValidEmail = new RegExp(sValidEmail)
-
-    var f = function (email) {
-      return reValidEmail.exec(email)
-    }
-
-    return f
-  }())
-
-  var match = init(email)
-
+module.exports = email => {
+  const match = reValidEmail.exec(email)
   return {
-    'local-part': (match !== null) ? match[1] : false,
-    'domain': (match !== null) ? match[7] : false
+    'local-part': match !== null ? match[1] : false,
+    'domain': match !== null ? match[7] : false
   }
 }
-
-module.exports = emailTools
