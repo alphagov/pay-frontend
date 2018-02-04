5f87325f6dbc9492f12b0397ff83656a2c70c2b1
maxcbc <maxcbc@live.com>
2018-02-04 18:26:27 +0000

    PP-3266 Refactored cookies util - switched to module level strict mode - extracted all calls to `process.env` into constants - refactored tests to handle the fact that all calls to process.env are made at module load

--- test/integration/util_cookies_ft_tests.js
+++ test/integration/util_cookies_ft_tests.js
@@ -1,18 +1,33 @@
-var assert = require('assert')
-var cookies = require('../../app/utils/cookies.js')
+'use strict'
+
+// npm dependencies
+const {expect} = require('chai')
+const proxyquire = require('proxyquire').noPreserveCache()
 
 describe('frontend cookie', function () {
+  let initialEnvironmentVariables
+  before(() => {
+    initialEnvironmentVariables = Object.assign({}, process.env)
+  })
+
+  afterEach(() => {
+    for (const envVar in process.env) {
+      process.env[envVar] = initialEnvironmentVariables[envVar]
+    }
+  })
   it('should have secure proxy off in unsecured environment', function () {
     process.env.SECURE_COOKIE_OFF = 'true'
-    assert.equal('frontend_state', cookies.getSessionCookieName())
-    assert.equal(true, cookies.namedCookie('name', 'key').proxy)
-    assert.deepEqual({httpOnly: true, secureProxy: false, maxAge: 5400000}, cookies.namedCookie('name', 'key').cookie)
+    const cookies = proxyquire('../../app/utils/cookies.js', {})
+    expect(cookies.getSessionCookieName()).to.equal('frontend_state')
+    expect(cookies.namedCookie('name', 'key').proxy).to.equal(true)
+    expect(cookies.namedCookie('name', 'key').cookie).to.deep.equal({httpOnly: true, secureProxy: false, maxAge: 5400000})
   })
 
   it('should have secure proxy on in a secured https environment', function () {
     process.env.SECURE_COOKIE_OFF = 'false'
-    assert.equal('frontend_state', cookies.getSessionCookieName())
-    assert.equal(true, cookies.namedCookie('name', 'key').proxy)
-    assert.deepEqual({httpOnly: true, secureProxy: true, maxAge: 5400000}, cookies.namedCookie('name', 'key').cookie)
+    const cookies = proxyquire('../../app/utils/cookies.js', {})
+    expect(cookies.getSessionCookieName()).to.equal('frontend_state')
+    expect(cookies.namedCookie('name', 'key').proxy).to.equal(true)
+    expect(cookies.namedCookie('name', 'key').cookie).to.deep.equal({httpOnly: true, secureProxy: true, maxAge: 5400000})
   })
 })
