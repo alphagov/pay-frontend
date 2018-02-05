1e992628d3e40e2ec9a9839bed4ad00ce1245bd6
maxcbc <maxcbc@live.com>
2018-02-05 18:38:14 +0000

    PP-3266 Delete accessible-autocomplete a) it's broken b) we don't use it

--- app/utils/accessible-autocomplete.js
+++
@@ -1,9 +0,0 @@
-var accessibleAutocomplete = require('accessible-autocomplete')
-
-var countryPicker = document.querySelector('#address-country')
-
-if (countryPicker) {
-  accessibleAutocomplete.enhanceSelectElement({
-    selectElement: countryPicker
-  })
-}
