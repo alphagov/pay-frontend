67ebfdc1bdf287ad90e36a9b8c6e40ec755467e3
maxcbc <maxcbc@live.com>
2018-02-06 09:07:32 +0000

    PP-3266 Refactored charge_validation util - switched to module level strict mode - removed unnecessary inner functions - prefer const

--- app/utils/charge_validation.js
+++ app/utils/charge_validation.js
@@ -1,6 +1,13 @@
-var chargeValidationFields = require('./charge_validation_fields')
-var changeCase = require('change-case')
-var customError = {
+'use strict'
+
+// npm dependencies
+const changeCase = require('change-case')
+
+// local dependencies
+const chargeValidationFields = require('./charge_validation_fields')
+
+// constants
+const CUSTOM_ERRORS = {
   expiryYear: {
     skip: true
   },
@@ -9,97 +16,41 @@ var customError = {
   }
 }
 
-module.exports = function (translations, logger, Card) {
-  'use strict'
-  var validations = chargeValidationFields(Card)
-  var fieldValidations = validations.fieldValidations
-  var requiredFields = validations.requiredFormFields
-  var optionalFields = validations.optionalFormFields
-
-  var creditCardType = validations.creditCardType
-  var verify = function (body) {
-    var checkResult = {
-      hasError: false,
-      errorFields: [],
-      highlightErrorFields: {}
-    }
-
-    var init = function () {
-      var field
-      for (var i = 0; i < requiredFields.length; i++) {
-        field = requiredFields[i]
-        checkRequiredFormField(field, body[field])
+module.exports = (translations, logger, Card) => {
+  const validations = chargeValidationFields(Card)
+  const fieldValidations = validations.fieldValidations
+  return {
+    required: validations.requiredFormFields,
+    optional: validations.optionalFormFields,
+    creditCardType: validations.creditCardType,
+    allowedCards: Card.allowed,
+    cardNo: fieldValidations.cardNo,
+    verify: (body) => {
+      const checkResult = {
+        hasError: false,
+        errorFields: [],
+        highlightErrorFields: {}
       }
-
-      for (var j = 0; j < optionalFields.length; j++) {
-        field = optionalFields[j]
-        if (body[field]) checkOptionalFormField(field, body[field])
-      }
+      validations.requiredFormFields.forEach(field => checkFormField(field, body[field], false))
+      validations.optionalFormFields.forEach(field => {
+        if (body[field]) checkFormField(field, body[field])
+      })
 
       if (checkResult.errorFields.length > 0) checkResult.hasError = true
       return checkResult
-    }
 
-    var checkRequiredFormField = function (name, value) {
-      var customValidation = checkFieldValidation(name, value)
-      if (value && customValidation === true) return
-      var translation = translations.fields[name]
-      var highlightMessage = translation.message
-      var problem = !value ? missing(name) : translation[customValidation]
-
-      pushToErrorFields(name, problem)
-      pushToHighlightField(name, highlightMessage)
-    }
-
-    var checkOptionalFormField = function (name, value) {
-      var customValidation = checkFieldValidation(name, value)
-      if (value && customValidation === true) return
-      var translation = translations.fields[name]
-      var highlightMessage = translation.message
-      var problem = translation[customValidation]
-
-      pushToErrorFields(name, problem)
-      pushToHighlightField(name, highlightMessage)
-    }
-
-    var checkFieldValidation = function (name, value) {
-      return (fieldValidations[name]) ? fieldValidations[name].bind(fieldValidations)(value, body) : true
-    }
-
-    var missing = function (name) {
-      var translation = translations.fields[name]
-      return 'Enter a valid ' + translation.name
-    }
-
-    var pushToErrorFields = function (fieldName, problem) {
-      var custom = customError[fieldName]
-      var cssKey = changeCase.paramCase(fieldName)
-      if (custom) {
-        if (custom.skip) return
-        if (custom.cssKey) cssKey = custom.cssKey
+      function checkFormField (name, value, isOptional) {
+        const customValidation = fieldValidations[name] ? fieldValidations[name].bind(fieldValidations)(value, body) : true
+        if (value && customValidation === true) return
+        const translation = translations.fields[name]
+        const problem = value || isOptional ? translation[customValidation] : `Enter a valid ${translation.name}`
+        // Push to Error Fields
+        const customError = CUSTOM_ERRORS[name] || {}
+        const cssKey = customError.cssKey || changeCase.paramCase(name)
+        if (!customError.skip) checkResult.errorFields.push({cssKey, key: name, value: problem})
+        // Push to Highlight Fields
+        checkResult.highlightErrorFields[name] = translation.message
       }
-
-      checkResult.errorFields.push({
-        cssKey: cssKey,
-        key: fieldName,
-        value: problem
-      })
     }
-
-    var pushToHighlightField = function (fieldName, highlightMessage) {
-      var fields = checkResult.highlightErrorFields
-      fields[fieldName] = highlightMessage
-    }
-
-    return init()
-  }
-
-  return {
-    verify: verify,
-    required: requiredFields,
-    optional: optionalFields,
-    creditCardType: creditCardType,
-    allowedCards: Card.allowed,
-    cardNo: fieldValidations.cardNo
   }
 }
