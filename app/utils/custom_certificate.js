289edb446360c93fa57c51481e8d43be738c30c9
maxcbc <maxcbc@live.com>
2018-02-06 10:11:34 +0000

    PP-3266 Updated analytics and custom_certificate utils to merge changes

--- app/utils/custom_certificate.js
+++ app/utils/custom_certificate.js
@@ -9,31 +9,24 @@ const logger = require('winston')
 // constants
 const CERTS_PATH = process.env.CERTS_PATH || path.join(__dirname, '/../../certs')
 exports.getCertOptions = (certsPath) => {
-    try {
-      if (!fs.lstatSync(certsPath).isDirectory()) {
-        logger.error('Provided CERTS_PATH is not a directory', {
-          certsPath: certsPath
-        })
-        return
-      }
-    } catch (e) {
-      logger.error('Provided CERTS_PATH could not be read', {
-        certsPath: certsPath
-      })
-      return
+  certsPath = certsPath || CERTS_PATH
+  try {
+    if (fs.lstatSync(certsPath).isDirectory()) {
+      // Read everything from the certificates directories
+      // Get everything that isn't a directory (e.g. files, symlinks)
+      // Read it (assume it is a certificate)
+      // Add it to the agentOptions list of CAs
+      const ca = []
+      fs.readdirSync(certsPath)
+        .map(certPath => path.join(certsPath, certPath))
+        .filter(fullCertPath => !fs.lstatSync(fullCertPath).isDirectory())
+        .map(fullCertPath => fs.readFileSync(fullCertPath))
+        .forEach(caCont => ca.push(caCont))
+      return ca
+    } else {
+      logger.error('Provided CERTS_PATH is not a directory', {certsPath})
     }
-
-    var ca = []
-    // Read everything from the certificates directories
-    // Get everything that isn't a directory (e.g. files, symlinks)
-    // Read it (assume it is a certificate)
-    // Add it to the agentOptions list of CAs
-    fs.readdirSync(certsPath)
-      .map(certPath => path.join(certsPath, certPath))
-      .filter(fullCertPath => !fs.lstatSync(fullCertPath).isDirectory())
-      .map(fullCertPath => fs.readFileSync(fullCertPath))
-      .forEach(caCont => ca.push(caCont))
-
-    return ca
+  } catch (e) {
+    logger.error('Provided CERTS_PATH could not be read', {certsPath})
   }
 }
