133c0c1fd4e86a863b868992c19c768641996c63
maxcbc <maxcbc@live.com>
2018-02-06 18:33:52 +0000

    PP-3266 Refactored charge_validation_fields util - switched to module level strict mode - extracted unnecessary constants and validator methods from within function so that they are not re-declared on each invocation - simplified validators - prefer const - updated babel config to work on IE - added babel-polyfill

--- app/utils/charge_validation_fields.js
+++ app/utils/charge_validation_fields.js
@@ -1,65 +1,34 @@
-var luhn = require('./luhn')
-var ukPostcode = require('uk-postcode')
-var creditCardType = require('credit-card-type')
-var rfc822Validator = require('rfc822-validate')
-var emailTools = require('./email_tools')
-var EMAIL_MAX_LENGTH = 254
+'use strict'
 
-module.exports = function (Card) {
-  'use strict'
-  var requiredFormFields = [
-    'cardNo',
-    'expiryMonth',
-    'expiryYear',
-    'cardholderName',
-    'cvc',
-    'addressLine1',
-    'addressCity',
-    'addressPostcode',
-    'email',
-    'addressCountry'
-  ]
+// npm dependencies
+const ukPostcode = require('uk-postcode')
+const rfc822Validator = require('rfc822-validate')
 
-  var optionalFormFields = [
-    'addressLine2'
-  ]
+// local dependencies
+const luhn = require('./luhn')
+const creditCardType = require('credit-card-type')
+const emailTools = require('./email_tools')
 
-  var validateEmail = function (email) {
-    var validEmail = rfc822Validator(email)
-    var domain
+// constants
+const EMAIL_MAX_LENGTH = 254
+const REQUIRED_FORM_FIELDS = [
+  'cardNo',
+  'expiryMonth',
+  'expiryYear',
+  'cardholderName',
+  'cvc',
+  'addressLine1',
+  'addressCity',
+  'addressPostcode',
+  'email',
+  'addressCountry'
+]
 
-    if (!validEmail) {
-      return 'message'
-    } else {
-      domain = emailTools(email).domain
-      if (domain && domain.indexOf('.') === -1) {
-        return 'message'
-      }
-      return true
-    }
-  }
+const OPTIONAL_FORM_FIELDS = [
+  'addressLine2'
+]
 
-  function containsSuspectedCVV (input) {
-    if (typeof input === 'number' || typeof input === 'string') {
-      return /^\s*\d{3,4}\s*$/.test(String(input))
-    } else {
-      return false
-    }
-  }
-
-  function containsSuspectedPAN (input) {
-    if (typeof input === 'number' || typeof input === 'string') {
-      var matchedDigits = String(input).match(/(\d)/g)
-      return (matchedDigits !== null) && (matchedDigits.length > 11)
-    } else {
-      return false
-    }
-  }
-
-  function isValidPostcode (postcode, countryCode) {
-    return !(countryCode === 'GB' && !ukPostcode.fromString(postcode).isComplete())
-  }
-
+module.exports = Card => {
   /*
    These are custom validations for each field.
    Functions should be named the same as the input name
@@ -67,97 +36,85 @@ module.exports = function (Card) {
    One is the field, second is allfields in case it is
    needed for validation.
    */
-
-  var fieldValidations = {
-    cardNo: function (cardNo) {
-      if (!cardNo) return 'message' // default message
-      cardNo = cardNo.replace(/\D/g, '')
-      var cardType = creditCardType(cardNo)
-      var valid = luhn.validate(cardNo)
-      if (!cardNo || cardNo.length < 12 || cardNo.length > 19) return 'number_incorrect_length'
-      if (!valid) return 'luhn_invalid'
-      if (!cardType[0]) return 'card_not_supported'
-      for (var i = 0; i < this.allowedCards.length; i++) {
-        if (this.allowedCards[i].brand === cardType[0].type) return true
-      }
-      return 'card_not_supported'
-    },
-
-    expiryMonth: function (expiryMonth, allFields) {
-      var expiryYear = allFields.expiryYear
-      if (expiryMonth === undefined || expiryMonth === '') return 'message'
-      if (expiryYear === undefined || expiryYear === '') return 'message'
-
-      // month is zero indexed
-      expiryMonth = expiryMonth - 1
-      var isValidMonth = /^\d+$/.test(expiryMonth) && expiryMonth >= 0 && expiryMonth <= 11
-      if (!isValidMonth) return 'invalid_month'
-      // validations are grouped for expiry month and year,
-      // this is why both validations are happening in expiry month
-      // can;t be renamed as it all runs off meta programming further up the stack
-      var isValidYear = (String(allFields.expiryYear).length === 2 || String(allFields.expiryYear).length === 4)
-      if (!isValidYear) return 'invalid_year'
-
-      var cardDate = allFields.expiryYear
-      if (String(allFields.expiryYear).length === 2) cardDate = '20' + cardDate
-      cardDate = new Date(cardDate, expiryMonth)
-
-      var currentDate = new Date()
-      if (currentDate.getFullYear() > cardDate.getFullYear()) return 'in_the_past'
-      if (currentDate.getFullYear() === cardDate.getFullYear() &&
-        currentDate.getMonth() > cardDate.getMonth()) return 'in_the_past'
-
-      return true
-    },
-
-    cvc: function (code) {
-      if (code === undefined) return 'invalid_length'
-      code = code.replace(/\D/g, '')
-      if (code.length === 3 || code.length === 4) {
-        return true
-      }
-      return 'invalid_length'
-    },
-
-    addressPostcode: function (addressPostcode, allFields) {
-      if (!isValidPostcode(addressPostcode, allFields.addressCountry)) return 'message'
-      if (containsSuspectedPAN(addressPostcode)) return 'contains_too_many_digits'
-      return true
-    },
-
-    email: function (email) {
-      if (email && email.length > EMAIL_MAX_LENGTH) return 'invalid_length'
-      if (containsSuspectedPAN(email)) return 'contains_too_many_digits'
-      return validateEmail(email)
-    },
-
-    cardholderName: function (name) {
-      if (containsSuspectedPAN(name)) return 'contains_too_many_digits'
-      if (containsSuspectedCVV(name)) return 'contains_suspected_cvv'
-      return true
-    },
-
-    addressLine1: function (addressLine) {
-      return !containsSuspectedPAN(addressLine) || 'contains_too_many_digits'
-    },
-
-    addressLine2: function (addressLine) {
-      return !containsSuspectedPAN(addressLine) || 'contains_too_many_digits'
-    },
-
-    addressCity: function (townCity) {
-      return !containsSuspectedPAN(townCity) || 'contains_too_many_digits'
-    },
-
-    creditCardType: creditCardType,
-    allowedCards: Card.allowed
-  }
-
   return {
     creditCardType: creditCardType,
     allowedCards: Card.allowed,
-    requiredFormFields: requiredFormFields,
-    fieldValidations: fieldValidations,
-    optionalFormFields: optionalFormFields
+    requiredFormFields: REQUIRED_FORM_FIELDS,
+    fieldValidations: {
+      cardNo: cardNo.bind(Card),
+      expiryMonth,
+      cvc,
+      addressPostcode,
+      email,
+      cardholderName,
+      addressLine1: addressLine,
+      addressLine2: addressLine,
+      addressCity: addressLine,
+      creditCardType: creditCardType,
+      allowedCards: Card.allowed
+    },
+    optionalFormFields: OPTIONAL_FORM_FIELDS
   }
+}
+
+// Validation Functions
+function expiryMonth (expiryMonth, allFields) {
+  let expiryYear = allFields.expiryYear
+  if (!expiryMonth || !expiryYear) return 'message'
+
+  // validations for both expiry month and year are done together in 'expiryMonth'
+  // it can't be renamed as it all runs off meta programming further up the stack
+  expiryMonth = expiryMonth - 1 // month is zero indexed
+  if (!(/^\d+$/.test(expiryMonth) && expiryMonth >= 0 && expiryMonth <= 11)) return 'invalid_month'
+  if (![2, 4].includes(String(allFields.expiryYear).length)) return 'invalid_year'
+  if (`${expiryYear}`.length === 2) expiryYear = '20' + expiryYear
+  const cardDate = new Date(expiryYear, expiryMonth)
+  const currentDate = new Date()
+  if (currentDate.getFullYear() > cardDate.getFullYear()) return 'in_the_past'
+  if (currentDate.getFullYear() === cardDate.getFullYear() && currentDate.getMonth() > cardDate.getMonth()) return 'in_the_past'
+
+  return true
+}
+function cvc (code) {
+  if (!code || ![3, 4].includes(code.replace(/\D/g, '').length)) return 'invalid_length'
+  return true
+}
+function addressPostcode (postcode, allFields) {
+  if (allFields.addressCountry === 'GB' && !ukPostcode.fromString(postcode).isComplete()) return 'message'
+  if (containsSuspectedPAN(postcode)) return 'contains_too_many_digits'
+  return true
+}
+function email (email) {
+  if (email && email.length > EMAIL_MAX_LENGTH) return 'invalid_length'
+  if (containsSuspectedPAN(email)) return 'contains_too_many_digits'
+  if (!rfc822Validator(email)) return 'message'
+  const domain = emailTools(email).domain
+  return domain && domain.indexOf('.') === -1 ? 'message' : true
+}
+function cardholderName (name) {
+  if (containsSuspectedPAN(name)) return 'contains_too_many_digits'
+  if (containsSuspectedCVV(name)) return 'contains_suspected_cvv'
+  return true
+}
+function addressLine (addressLine) {
+  return !containsSuspectedPAN(addressLine) || 'contains_too_many_digits'
+}
+function containsSuspectedPAN (input) {
+  if (!['string', 'number'].includes(typeof input)) return false
+  const matchedDigits = String(input).match(/(\d)/g)
+  return (matchedDigits !== null) && (matchedDigits.length > 11)
+}
+function containsSuspectedCVV (input) {
+  if (!['string', 'number'].includes(typeof input)) return false
+  return /^\s*\d{3,4}\s*$/.test(String(input))
+}
+// Must be bound prior to use
+function cardNo (cardNo) {
+  if (!cardNo) return 'message' // default message
+  cardNo = cardNo.replace(/\D/g, '')
+  const cardType = creditCardType(cardNo)
+  if (!cardNo || cardNo.length < 12 || cardNo.length > 19) return 'number_incorrect_length'
+  if (!luhn.validate(cardNo)) return 'luhn_invalid'
+  if (!cardType[0]) return 'card_not_supported'
+  return this.allowed.map(card => card.brand).includes(cardType[0].type) || 'card_not_supported'
 }
