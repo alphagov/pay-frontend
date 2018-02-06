f4ab7d100b05e60d8e94b32e01d0ab5996c43958
maxcbc <maxcbc@live.com>
2018-02-06 10:11:34 +0000

    PP-3266 Updated analytics and custom_certificate utils to merge changes

--- app/utils/analytics.js
+++ app/utils/analytics.js
@@ -16,25 +16,14 @@ const ANALYTICS_ERROR = {
   }
 }
 
-module.exports = (function () {
-  'use strict'
-
-  var withAnalytics = function (charge, param) {
-    return _.merge(
-      {
-        analytics: {
-          path: paths.generateRoute(`card.new`, {chargeId: charge.id}),
-          analyticsId: charge.gatewayAccount.analyticsId,
-          type: charge.gatewayAccount.type,
-          paymentProvider: charge.gatewayAccount.paymentProvider,
-          amount: charge.amount
-        }}, param)
-  }
-  var withError = function () {
-    return _.clone(ANALYTICS_ERROR)
-  }
-  return {
-    withAnalyticsError: withError,
-    withAnalytics: withAnalytics
-  }
-}())
+exports.withAnalyticsError = () => lodash.clone(ANALYTICS_ERROR)
+exports.withAnalytics = (charge, param) => {
+  return lodash.merge({
+    analytics: {
+      path: paths.generateRoute(`card.new`, {chargeId: charge.id}),
+      analyticsId: charge.gatewayAccount.analyticsId,
+      type: charge.gatewayAccount.type,
+      paymentProvider: charge.gatewayAccount.paymentProvider,
+      amount: charge.amount
+    }}, param)
+}
