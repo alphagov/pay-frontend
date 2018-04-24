a984ece5a8955718a0c31f6cdfbe1475bab7fc90
Jon Heslop <jon@jonheslop.com>
2018-04-24 12:43:56 +0100

    PP-3225 - Was including the script twice
    One of which was not transpiled so was causing all sorts of JS errors :s

--- app/browsered.js
+++ app/browsered.js
@@ -1,6 +1,6 @@
 require('babel-polyfill')
 
-const inputConfim = require('./assets/javascripts/modules/form-input-confirm')
+const inputConfim = require('./assets/javascripts/browsered/form-input-confirm')
 
 window.jQuery = window.$ = require('jquery')
 exports.chargeValidation = require('./utils/charge_validation')
