da1b61a44e0496d8faedfff8caf0d15961408c6b
Jon Heslop <jon@jonheslop.com>
2018-08-29 10:18:25 +0100

    PP-4098 - Get service name translation
    Previously we were getting the service name from the gateway account
    object now getting it from the service object (which comes from
    adminusers).
    
    Pass the language to the template and use that to get the correct
    service name translation.

--- app/middleware/resolve_language.js
+++ app/middleware/resolve_language.js
@@ -7,5 +7,6 @@ module.exports = function (req, res, next) {
   const language = req.chargeData.language || 'en'
   i18n.setLocale(req, language)
   res.locals.translationStrings = JSON.stringify(i18n.getCatalog(language))
+  res.locals.language = language
   next()
 }
