c379a875241ac573a413ead342542759515bc7d3
Danail Minchev <danail.minchev@gmail.com>
2018-10-30 11:26:14 +0000

    PP-4205 Move redirectToServiceImmediatelyOnTerminalState property to correct place
    with @stephencdaly

--- app/models/Service.class.js
+++ app/models/Service.class.js
@@ -23,19 +23,21 @@ class Service {
     this.externalId = serviceData.external_id
     this.name = serviceData.service_name
     this.gatewayAccountIds = serviceData.gateway_account_ids
+    this.redirectToServiceImmediatelyOnTerminalState = serviceData.redirect_to_service_immediately_on_terminal_state
+
     this.customBranding =
       serviceData.custom_branding ? {
         cssUrl: serviceData.custom_branding.css_url,
         imageUrl: serviceData.custom_branding.image_url
       } : undefined
+
     this.merchantDetails = serviceData.merchant_details ? {
       name: serviceData.merchant_details.name,
       addressLine1: serviceData.merchant_details.address_line1,
       addressLine2: serviceData.merchant_details.address_line2,
       city: serviceData.merchant_details.address_city,
       postcode: serviceData.merchant_details.address_postcode,
-      countryName: countries.translateCountryISOtoName(serviceData.merchant_details.address_country),
-      redirectToServiceImmediatelyOnTerminalState: serviceData.redirect_to_service_immediately_on_terminal_state
+      countryName: countries.translateCountryISOtoName(serviceData.merchant_details.address_country)
     } : undefined
   }
 
