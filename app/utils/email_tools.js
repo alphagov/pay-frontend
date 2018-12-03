6b246f8df8cbefc11ef42083db096f84012d197f
Jon Heslop <jon@jonheslop.com>
2018-12-03 10:36:01 +0000

    PP-4480 - add .im domain to whitelist

--- app/utils/email_tools.js
+++ app/utils/email_tools.js
@@ -32,7 +32,7 @@ const validEmail = email => {
 mailcheck.defaultDomains = ['live.com', 'live.co.uk', 'live.com.au', 'gmail.com', 'gmail.co.uk', 'ymail.com', 'mail.com', 'hotmail.com']
 mailcheck.defaultSecondLevelDomains = ['yahoo', 'outlook', 'hotmail']
 mailcheck.defaultTopLevelDomains = [
-  'com', 'ca', 'co.nz', 'co.uk', 'de', 'fr', 'it', 'ru', 'net', 'org', 'edu', 'gov', 'jp', 'nl', 'kr', 'se', 'eu', 'ie', 'co.il', 'us', 'at', 'be', 'dk', 'hk', 'es', 'gr', 'ch', 'no', 'cz', 'in', 'net', 'net.au', 'info', 'biz', 'mil', 'co.jp', 'sg', 'hu', 'uk', 'ac.uk', 'co.uk', 'gov.uk', 'judiciary.uk', 'ltd.uk', 'me.uk', 'mod.uk', 'net.uk', 'nhs.uk', 'nic.uk', 'org.uk', 'plc.uk', 'police.uk', 'sch.uk', 'ae', 'net.ae', 'pl', 'kw', 'co', 'aero', 'co.in', 'io', 'bz', 'bg', 'sk', 'sx', 'dm', 'bm', 'om', 'me', 'qa', 'coop', 'fm', 'cc', 'mc', 'my', 'vc'
+  'com', 'ca', 'co.nz', 'co.uk', 'de', 'fr', 'it', 'ru', 'net', 'org', 'edu', 'gov', 'jp', 'nl', 'kr', 'se', 'eu', 'ie', 'im', 'co.il', 'us', 'at', 'be', 'dk', 'hk', 'es', 'gr', 'ch', 'no', 'cz', 'in', 'net', 'net.au', 'info', 'biz', 'mil', 'co.jp', 'sg', 'hu', 'uk', 'ac.uk', 'co.uk', 'gov.uk', 'judiciary.uk', 'ltd.uk', 'me.uk', 'mod.uk', 'net.uk', 'nhs.uk', 'nic.uk', 'org.uk', 'plc.uk', 'police.uk', 'sch.uk', 'ae', 'net.ae', 'pl', 'kw', 'co', 'aero', 'co.in', 'io', 'bz', 'bg', 'sk', 'sx', 'dm', 'bm', 'om', 'me', 'qa', 'coop', 'fm', 'cc', 'mc', 'my', 'vc'
 ]
 mailcheck.domainThreshold = 2
 mailcheck.secondLevelThreshold = 2
