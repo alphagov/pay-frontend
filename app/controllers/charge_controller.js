fcd9a3028b7aff9587d86c6bc4267d58e807f7e2
Jon Heslop <jon@jonheslop.com>
2019-02-12 12:40:33 +0000

    PP-4784 - use new separate flags for Apple Pay and Google Pay

--- app/controllers/charge_controller.js
+++ app/controllers/charge_controller.js
@@ -40,8 +40,8 @@ const appendChargeForNewView = (charge, req, chargeId) => {
   charge.post_card_action = routeFor('create', chargeId)
   charge.id = chargeId
   charge.post_cancel_action = routeFor('cancel', chargeId)
-  charge.allowApplePay = charge.gatewayAccount.allowWebPayments
-  charge.allowGooglePay = charge.gatewayAccount.allowWebPayments
+  charge.allowApplePay = charge.gatewayAccount.allow_apple_pay
+  charge.allowGooglePay = charge.gatewayAccount.allow_google_pay
   charge.stubsUrl = process.env.APPLE_PAY_STUBS_URL
 }
 
