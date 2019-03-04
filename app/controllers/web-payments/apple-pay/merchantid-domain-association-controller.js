3af303157c625e7e9d4395014dc4d1eeb18792fc
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-03-04 12:35:29 +0000

    Update app/controllers/web-payments/apple-pay/merchantid-domain-association-controller.js
    Co-Authored-By: 46bit <hi@46b.it>

--- app/controllers/web-payments/apple-pay/merchantid-domain-association-controller.js
+++ app/controllers/web-payments/apple-pay/merchantid-domain-association-controller.js
@@ -7,7 +7,7 @@ const logger = require('winston')
 const { APPLE_PAY_MERCHANT_ID_DOMAIN_ASSOCIATION } = process.env
 
 module.exports = (req, res) => {
-  if (APPLE_PAY_MERCHANT_ID_DOMAIN_ASSOCIATION.length === 0) {
+  if (APPLE_PAY_MERCHANT_ID_DOMAIN_ASSOCIATION && APPLE_PAY_MERCHANT_ID_DOMAIN_ASSOCIATION.length === 0) {
     logger.warn(`Apple Pay domain assocation returned 404 as the environment variable was not set`)
     return res.sendStatus(404)
   }
