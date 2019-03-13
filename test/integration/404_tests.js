806b04cbd98ccf97688b55059ef2430133a23f62
Jon Heslop <jon@jonheslop.com>
2019-03-13 15:35:59 +0000

    PP-4924 - update test to use asset that still exists

--- test/integration/404_tests.js
+++ test/integration/404_tests.js
@@ -1,6 +1,6 @@
 const request = require('supertest')
 const app = require('../../server.js').getApp()
-const expect = require('chai').expect
+const { expect } = require('chai')
 
 describe('Invalid pages redirect to 404 page', function () {
   it('should return 302 to gov.uk when not found', function (done) {
@@ -21,7 +21,7 @@ describe('Invalid pages redirect to 404 page', function () {
 
   it('should not return 302 to gov.uk when static', function (done) {
     request(app)
-      .get('/public/images/acc.png')
+      .get('/public/images/spinner.gif')
       .expect(200)
       .expect(res => expect(res.headers['location']).to.not.equal('https://www.gov.uk/404'))
       .end(done)
