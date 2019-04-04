5e4af78c4d6284da7b25d1acfb2ddad78633beb0
Jon Heslop <jon@jonheslop.com>
2019-04-04 17:24:22 +0100

    PP-5072 - Add test for browser with Apple Pay for service that doesn’t use Apple Pay

--- test/cypress/integration/card/payment.spec.js
+++ test/cypress/integration/card/payment.spec.js
@@ -1,4 +1,5 @@
 const cardPaymentStubs = require('../../utils/card-payment-stubs')
+const { getMockApplePayClass } = require('../../utils/apple-pay-js-api-stubs')
 
 describe('Standard card payment flow', () => {
   const tokenId = 'be88a908-3b99-4254-9807-c855d53f6b2b'
@@ -203,6 +204,75 @@ describe('Standard card payment flow', () => {
 
       cy.get('#card-no').should('have.class', 'govuk-input--error')
       cy.get('#card-no-lbl').should('contain', 'Nid yw rhif y cerdyn yr hyd cywir')
+    })
+  })
+
+  describe('Secure card payment page on Apple Pay enabled browser but not Apple Pay Enabled service', () => {
+    it('Should setup the payment and load the page', () => {
+      cy.task('setupStubs', createPaymentChargeStubsEnglish)
+      cy.visit(`/secure/${tokenId}`)
+
+      // 1. Charge will be created using this id as a token (GET)
+      // 2. Token will be deleted (DELETE)
+      // 3. Charge will be fetched (GET)
+      // 4. Service related to charge will be fetched (GET)
+      // 5. Charge status will be updated (PUT)
+      // 6. Client will be redirected to /card_details/:chargeId (304)
+      cy.location('pathname').should('eq', `/card_details/${chargeId}`)
+    })
+
+    it('Should enter and validate a correct card', () => {
+      cy.task('setupStubs', checkCardDetailsStubs)
+
+      cy.visit(`/card_details/${chargeId}`, {
+        onBeforeLoad: win => {
+          // Stub Apple Pay API (which only exists within Safari)
+          win.ApplePaySession = getMockApplePayClass('jonheslop@bla.test')
+        }
+      })
+
+      cy.server()
+      cy.route('POST', `/check_card/${chargeId}`).as('checkCard')
+
+      cy.get('#card-no').type(validPayment.cardNumber)
+      cy.get('#card-no').blur()
+
+      // 7. Charge status will be fetched (GET)
+      // 8. CardID POST to verify that entered card is correct - this is configured to return valid
+      cy.wait('@checkCard')
+      cy.get('#card-no').should('not.have.class', 'govuk-input--error')
+    })
+
+    it('Should enter payment details', () => {
+      cy.get('#expiry-month').type(validPayment.expiryMonth)
+      cy.get('#expiry-year').type(validPayment.expiryYear)
+      cy.get('#cardholder-name').type(validPayment.name)
+      cy.get('#cvc').type(validPayment.securityCode)
+      cy.get('#address-line-1').type(validPayment.addressLine1)
+      cy.get('#address-city').type(validPayment.city)
+      cy.get('#address-postcode').type(validPayment.postcode)
+      cy.get('#email').type(validPayment.email)
+    })
+  })
+
+  describe('Secure confirmation page', () => {
+    it('Submitting confirmation with valid details should redirect to confirmation page', () => {
+      const lastFourCardDigits = validPayment.cardNumber.substr(-4)
+
+      cy.task('setupStubs', confirmPaymentDetailsStubs)
+      cy.get('#card-details').submit()
+
+      // 9. Charge status will be fetched - this will report the same as earlier as nothing has changed (GET)
+      // 10. CardID POST to verify that entered card is correct - this shouldn't have changed from earlier (POST)
+      // 11. Update charge details on Connector with valid card details (PATCH)
+      // 12. Create actual charge for the card details on Connector (expects success message) (POST)
+      // 13. Get charge status after authorisation (GET)
+      cy.location('pathname').should('eq', `/card_details/${chargeId}/confirm`)
+
+      // TD inner values are padded with white space - generic match
+      cy.get('#card-number').should(($td) => expect($td).to.contain(`************${lastFourCardDigits}`))
+      cy.get('#cardholder-name').should(($td) => expect($td).to.contain(validPayment.name))
+      cy.get('#email').should(($td) => expect($td).to.contain(validPayment.email))
     })
   })
 })
