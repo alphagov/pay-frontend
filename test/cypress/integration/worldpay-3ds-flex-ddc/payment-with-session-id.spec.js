32306409a9016bc06c80e366eba0027e84b33faa
danworth <dan.worth@digital.cabinet-office.gov.uk>
2019-09-11 13:06:28 +0100

    PP-5548 Add timeout for worldpay 3ds flex call
    If worldpay 3ds flex ddc does not respond to the iframe form POST
    after 10 seconds then submit the card details form without the sessionId form
    element. Includes cypress test.
    
    with @jonheslop

--- test/cypress/integration/worldpay-3ds-flex-ddc/payment-with-session-id.spec.js
+++ test/cypress/integration/worldpay-3ds-flex-ddc/payment-with-session-id.spec.js
@@ -1,6 +1,6 @@
 const cardPaymentStubs = require('../../utils/card-payment-stubs')
 
-describe('Standard card payment flow', () => {
+describe('Worldpay 3ds flex card payment flow', () => {
   const tokenId = 'be88a908-3b99-4254-9807-c855d53f6b2b'
   const chargeId = 'ub8de8r5mh4pb49rgm1ismaqfv'
   const sessionId = 'test session Id'
@@ -39,8 +39,6 @@ describe('Standard card payment flow', () => {
     { name: 'cardIdValidCardDetails' }
   ]
 
-  // @TODO(sfount) test the payment in progress in flows by returning statuses that indicate different levels of progress
-  // i.e - charge after or before authorisation when clicking confirm should bring up confirmation page or 'your payment is in progress' page respectively
   const confirmPaymentDetailsStubs = [
     { name: 'adminUsersGetService', opts: {} },
     {
@@ -57,9 +55,12 @@ describe('Standard card payment flow', () => {
       }]
     },
     { name: 'cardIdValidCardDetails' },
-    { name: 'connectorValidPatchConfirmedChargeDetails', opts: { chargeId } },
-    { name: 'worldpay3dsflexddcIframePost', opts: { sessionId } }
+    { name: 'connectorValidPatchConfirmedChargeDetails', opts: { chargeId } }
   ]
+
+  const worldpay3dsFlexDdcStub = {
+    name: 'worldpay3dsflexddcIframePost', opts: { sessionId }
+  }
 
   beforeEach(() => {
     // this test is for the full process, the session should be maintained
@@ -67,54 +68,70 @@ describe('Standard card payment flow', () => {
     Cypress.Cookies.preserveOnce('frontend_state')
   })
 
-  describe('Secure card payment page', () => {
-    it('Should setup the payment and load the page', () => {
-      cy.task('setupStubs', createPaymentChargeStubsEnglish)
-      cy.visit(`/secure/${tokenId}`)
+  const setUpAndCheckCardPaymentPage = () => {
+    describe('Secure card payment page', () => {
+      it('Should setup the payment and load the page', () => {
+        cy.task('setupStubs', createPaymentChargeStubsEnglish)
+        cy.visit(`/secure/${tokenId}`)
 
-      // 1. Charge will be created using this id as a token (GET)
-      // 2. Token will be deleted (DELETE)
-      // 3. Charge will be fetched (GET)
-      // 4. Service related to charge will be fetched (GET)
-      // 5. Charge status will be updated (PUT)
-      // 6. Client will be redirected to /card_details/:chargeId (304)
-      cy.location('pathname').should('eq', `/card_details/${chargeId}`)
+        // 1. Charge will be created using this id as a token (GET)
+        // 2. Token will be deleted (DELETE)
+        // 3. Charge will be fetched (GET)
+        // 4. Service related to charge will be fetched (GET)
+        // 5. Charge status will be updated (PUT)
+        // 6. Client will be redirected to /card_details/:chargeId (304)
+        cy.location('pathname').should('eq', `/card_details/${chargeId}`)
+      })
+
+      it('Should enter and validate a correct card', () => {
+        cy.task('setupStubs', checkCardDetailsStubs)
+
+        cy.server()
+        cy.route('POST', `/check_card/${chargeId}`).as('checkCard')
+
+        cy.get('#card-no').type(validPayment.cardNumber)
+        cy.get('#card-no').blur()
+
+        // 7. Charge status will be fetched (GET)
+        // 8. CardID POST to verify that entered card is correct - this is configured to return valid
+        cy.wait('@checkCard')
+        cy.get('#card-no').should('not.have.class', 'govuk-input--error')
+      })
+
+      it('Should enter payment details', () => {
+        cy.get('#expiry-month').type(validPayment.expiryMonth)
+        cy.get('#expiry-year').type(validPayment.expiryYear)
+        cy.get('#cardholder-name').type(validPayment.name)
+        cy.get('#cvc').type(validPayment.securityCode)
+        cy.get('#address-line-1').type(validPayment.addressLine1)
+        cy.get('#address-city').type(validPayment.city)
+        cy.get('#address-postcode').type(validPayment.postcode)
+        cy.get('#email').type(validPayment.email)
+      })
     })
+  }
 
-    it('Should enter and validate a correct card', () => {
-      cy.task('setupStubs', checkCardDetailsStubs)
-
-      cy.server()
-      cy.route('POST', `/check_card/${chargeId}`).as('checkCard')
-
-      cy.get('#card-no').type(validPayment.cardNumber)
-      cy.get('#card-no').blur()
-
-      // 7. Charge status will be fetched (GET)
-      // 8. CardID POST to verify that entered card is correct - this is configured to return valid
-      cy.wait('@checkCard')
-      cy.get('#card-no').should('not.have.class', 'govuk-input--error')
-    })
-
-    it('Should enter payment details', () => {
-      cy.get('#expiry-month').type(validPayment.expiryMonth)
-      cy.get('#expiry-year').type(validPayment.expiryYear)
-      cy.get('#cardholder-name').type(validPayment.name)
-      cy.get('#cvc').type(validPayment.securityCode)
-      cy.get('#address-line-1').type(validPayment.addressLine1)
-      cy.get('#address-city').type(validPayment.city)
-      cy.get('#address-postcode').type(validPayment.postcode)
-      cy.get('#email').type(validPayment.email)
-    })
-  })
-
+  setUpAndCheckCardPaymentPage()
   describe('Secure confirmation page', () => {
     it('Submitting confirmation with valid details and a JWT present should cause worldpay iFrame to pass extra input when submitting with the session ID', () => {
-      cy.task('setupStubs', confirmPaymentDetailsStubs)
+      cy.task('setupStubs', [...confirmPaymentDetailsStubs, worldpay3dsFlexDdcStub])
 
       cy.get('#card-details').submit().should($form => {
         const formVal = $form.first()[0].elements.worldpay3dsFlexDdcResult.value
         expect(formVal).to.eq(sessionId)
+      })
+    })
+  })
+
+  setUpAndCheckCardPaymentPage()
+  describe('Secure confirmation page', () => {
+    it('Submitting confirmation when Worldpay does not respond to iframe post should submit the form without the sessionId', () => {
+      confirmPaymentDetailsStubs.pop()
+      cy.task('setupStubs', confirmPaymentDetailsStubs)
+
+      cy.get('#card-details').submit().should($form => {
+        const formVal = $form.first()[0].elements.worldpay3dsFlexDdcResult
+        expect(formVal).to.eq(undefined)
       })
     })
   })
