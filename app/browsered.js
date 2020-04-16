691184b63006099abddaf112f9ea3a6784381833
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2020-04-16 17:28:22 +0100

    Add files

--- app/browsered.js
+++ app/browsered.js
@@ -2,14 +2,13 @@ const inputConfirm = require('./assets/javascripts/browsered/form-input-confirm'
 const webPayments = require('./assets/javascripts/browsered/web-payments')
 const analytics = require('gaap-analytics')
 // const formValidation = require('./assets/javascripts/browsered/form-validation')
-const helpers = require('./assets/javascripts/browsered/helpers')
+// const helpers = require('./assets/javascripts/browsered/helpers')
 
 exports.chargeValidation = require('./utils/charge_validation')
 analytics.eventTracking.init()
 
 // Place functions into scope so can trigger in scripts.njk
 window.payScripts = { // eslint-disable-line no-unused-vars
-  helpers,
   inputConfirm,
   webPayments
 }
