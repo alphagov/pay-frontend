6cf655a7c2637e5bd738fdab1a45b8de4244903a
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2020-11-27 15:06:14 +0000

    PP-7436 User research custom changes
    - Charge page
      - Move summary to the top of the page
      - Move card icons above the card field
      - Remove inline validation.
    - Confirm page
      - Move summary to the top of the page.

--- app/browsered.js
+++ app/browsered.js
@@ -1,19 +1,19 @@
 const inputConfirm = require('./assets/javascripts/browsered/form-input-confirm')
 const webPayments = require('./assets/javascripts/browsered/web-payments')
 const analytics = require('gaap-analytics')
-const formValidation = require('./assets/javascripts/browsered/form-validation')
+// const formValidation = require('./assets/javascripts/browsered/form-validation')
 const epdq3ds2 = require('./assets/javascripts/browsered/epdq-3ds2')
-const helpers = require('./assets/javascripts/browsered/helpers')
+// const helpers = require('./assets/javascripts/browsered/helpers')
 
 exports.chargeValidation = require('./utils/charge_validation')
 analytics.eventTracking.init()
 
 // Place functions into scope so can trigger in scripts.njk
 window.payScripts = { // eslint-disable-line no-unused-vars
-  helpers,
+  // helpers,
   inputConfirm,
   webPayments,
-  formValidation,
+  // formValidation,
   epdq3ds2
 }
 
