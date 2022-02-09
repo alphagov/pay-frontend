7b766f954db0314cd9de177e04fb6eec622b0d99
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2022-02-09 17:35:27 +0000

    PP-9187 remove appmetrics
    - Appmetrics is no longer maintained, and contains high severity
      vulnerabilities
    - It’s currently blocking an upgrade of the (currently vulnerable)
      alpine base image on the Node apps
    - Update package-lock

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// Please leave here even though it looks unused - this enables Node.js metrics to be pushed to Hosted Graphite
-if (process.env.DISABLE_APPMETRICS !== 'true') {
-  require('./app/utils/metrics.js').metrics()
-}
-
 // Node.js core dependencies
 const path = require('path')
 const crypto = require('crypto')
