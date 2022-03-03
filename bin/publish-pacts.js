8a346fdfeaadddbd907cec74f2f4ccfdb5b924b2
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2022-03-03 14:58:34 +0000

    PP-9153 Move pact publish logic to pact publish script
    Previously we relied on the 'run-and-test' shell script to
    ensure clean publication of pact files. This was a bad idea,
    but also that file no longer exists, so move logic to the
    publish script.

--- bin/publish-pacts.js
+++ bin/publish-pacts.js
@@ -1,14 +1,41 @@
 #!/usr/bin/env node
+const {unlink, readdir, exists} = require('fs').promises
+
 const pact = require('@pact-foundation/pact-node')
-const opts = {
-  pactFilesOrDirs: [`${__dirname}/../pacts/`],
-  pactBroker: process.env.PACT_BROKER_URL,
-  consumerVersion: process.env.PACT_CONSUMER_VERSION,
-  pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
-  pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
-  tags: process.env.PACT_CONSUMER_TAG
+
+const pactDirPath = `${__dirname}/../pacts/`
+
+async function publish() {
+  const opts = {
+    pactFilesOrDirs: [pactDirPath],
+    pactBroker: process.env.PACT_BROKER_URL,
+    consumerVersion: process.env.PACT_CONSUMER_VERSION,
+    pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
+    pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
+    tags: process.env.PACT_CONSUMER_TAG
+  }
+
+  await pact.publishPacts(opts)
+  console.log('Pacts published')
 }
 
-pact.publishPacts(opts).then(function () {
-  console.log('>> Pact files have been published')
-})
+async function removePactsNotToBePublished () {
+  const files = await readdir(pactDirPath)
+  //todo: fix this 'to-be' nonsense
+  files
+    .filter(filename => filename.includes('to-be'))
+    .forEach(async filename => await unlink(pactDirPath + filename))
+}
+
+async function run() {
+  //make sure pact dir exists, and clean out any files marked to be
+  if (await exists(pactDirPath)) {
+    await removePactsNotToBePublished()
+    await publish()
+  } else {
+    console.log('No pacts found')
+  }
+}
+
+run()
+
