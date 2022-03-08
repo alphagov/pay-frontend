608d984b4bff486c9a1035690418ff2859a3bd17
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-03-08 17:01:09 +0000

    added test for non-present headers

--- test/services/normalise-charge.test.js
+++ test/services/normalise-charge.test.js
@@ -8,36 +8,60 @@ describe('normalise', function () {
     expect(normalise.expiryDate('01', '2017')).to.eql('01/17')
   })
 
+  const card = '4242424242424242'
+
+  const bodyFixture = {
+    chargeId: '42mdrsshtsk4chpeoifhlgf4lk',
+    cardNo: card,
+    expiryMonth: '01',
+    expiryYear: '20',
+    cardholderName: 'Joe Bloggs',
+    cvc: '111',
+    addressCountry: 'GB',
+    addressLine1: '1 Horse Guards',
+    addressCity: 'London',
+    addressPostcode: 'E1 8QS'
+  }
+
   it('includes headers in payload', function () {
-    const headers =    {
-      'accept': 'application/json',
+    const headers = {
+      accept: 'application/json',
       'x-request-id': 'unique-id',
       'x-forwarded-for': '127.0.0.1',
       'user-agent': 'Mozilla/5.0',
       'accept-language': 'fr;q=0.9, fr-CH;q=1.0, en;q=0.8, de;q=0.7, *;q=0.5'
     }
-    const card = '4242424242424242'
-    
-    const payload =   { body: {
-      "chargeId": "42mdrsshtsk4chpeoifhlgf4lk",
-      "cardNo": card,
-      "expiryMonth": "01",
-      "expiryYear": "20",
-      "cardholderName": "Joe Bloggs",
-      "cvc": "111",
-      "addressCountry": "GB",
-      "addressLine1": "1 Horse Guards",
-      "addressCity": "London",
-      "addressPostcode": "E1 8QS" },
+
+    const payload = {
+      body: bodyFixture,
       header: headerName => {
         return headers[headerName]
       },
       headers: headers
     }
 
-    expect(normalise.apiPayload(payload, card).accept_language_header).to.eql('fr;q=0.9, fr-CH;q=1.0, en;q=0.8, de;q=0.7, *;q=0.5');
-    expect(normalise.apiPayload(payload, card).accept_header).to.eql('application/json');
-    expect(normalise.apiPayload(payload, card).user_agent_header).to.eql('Mozilla/5.0');
+    expect(normalise.apiPayload(payload, card).accept_language_header).to.eql('fr;q=0.9, fr-CH;q=1.0, en;q=0.8, de;q=0.7, *;q=0.5')
+    expect(normalise.apiPayload(payload, card).accept_header).to.eql('application/json')
+    expect(normalise.apiPayload(payload, card).user_agent_header).to.eql('Mozilla/5.0')
   })
 
+  it('sets not present headers to undefined', function () {
+    const headers = {
+      'x-request-id': 'unique-id',
+      'x-forwarded-for': '127.0.0.1'
+    }
+    const card = '4242424242424242'
+
+    const payload = {
+      body: bodyFixture,
+      header: headerName => {
+        return headers[headerName]
+      },
+      headers: headers
+    }
+
+    expect(normalise.apiPayload(payload, card).accept_language_header).to.eql(undefined)
+    expect(normalise.apiPayload(payload, card).accept_header).to.eql(undefined)
+    expect(normalise.apiPayload(payload, card).user_agent_header).to.eql(undefined)
+  })
 })
