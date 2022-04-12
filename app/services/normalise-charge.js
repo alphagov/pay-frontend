1708825fdfe612211ec939aef0e0981202c84fe9
Jerome Ballestraz <jerome@fintechorg.co.uk>
2022-04-12 17:06:36 +0100

    PP-9333: Front end changes for displaying agreement details

--- app/services/normalise-charge.js
+++ app/services/normalise-charge.js
@@ -23,6 +23,13 @@ module.exports = (function () {
       moto: charge.moto,
       gatewayAccount: _normaliseGatewayAccountDetails(charge.gateway_account)
     }
+
+    if(charge.agreement_id) {
+      chargeObj.agreementId = charge.agreement_id
+    }
+    if(charge.save_payment_instrument_to_agreement) {
+      chargeObj.savePaymentInstrumentToAgreement = charge.save_payment_instrument_to_agreement
+    }
     if (charge.auth_3ds_data) {
       chargeObj.auth3dsData = _normaliseAuth3dsData(charge.auth_3ds_data)
     }
@@ -39,6 +46,7 @@ module.exports = (function () {
     if (charge.wallet_type) {
       chargeObj.walletType = charge.wallet_type
     }
+
     return chargeObj
   }
 
