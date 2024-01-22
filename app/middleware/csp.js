bf82bf5286e1ad9dacba1d330868f84bb1c34007
Iqbal AHmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2024-01-22 17:23:57 +0000

    PP-11850 Reverting CSP change that was causing Naxsi issues
    - Removing the code in the content security policy that added code for
      `report-to`
    - Was causing an Naxsi issue.

--- app/middleware/csp.js
+++ app/middleware/csp.js
@@ -31,8 +31,6 @@ const styleSourceCardDetails = ['\'self\'', '\'unsafe-eval\'', '\'unsafe-inline\
 const formActionWP3DS = ['\'self\'', 'https://centinelapi.cardinalcommerce.com/V1/Cruise/Collect',
   'https://secure-test.worldpay.com/shopper/3ds/ddc.html']
 
-const CSP_REPORTING_ENDPOINT_NAME = 'csp-endpoint'
-
 // Direct redirect use case lets post to any given site
 const formActionCardDetails = (req, res) => {
   if (res.locals && res.locals.service &&
@@ -52,14 +50,9 @@ const connectSourceCardDetails = ['\'self\'', 'https://www.google-analytics.com'
 
 const skipSendingCspHeader = (req, res, next) => { next() }
 
-const setResponseCspReportingEndpointName = (req, res, next) => {
-  res.setHeader('Reporting-Endpoints', `${CSP_REPORTING_ENDPOINT_NAME}="${paths.csp.path}"`)
-  next()
-}
-
 const cardDetailsCSP = helmet.contentSecurityPolicy({
   directives: {
-    reportTo: CSP_REPORTING_ENDPOINT_NAME,
+    reportUri: paths.csp.path,
     frameSrc: frameAndChildSourceCardDetails,
     childSrc: frameAndChildSourceCardDetails,
     imgSrc: imgSourceCardDetails,
@@ -80,7 +73,7 @@ const cardDetailsCSP = helmet.contentSecurityPolicy({
 
 const worldpayIframeCSP = helmet.contentSecurityPolicy({
   directives: {
-    reportTo: CSP_REPORTING_ENDPOINT_NAME,
+    reportUri: paths.csp.path,
     defaultSrc: CSP_NONE,
     formAction: formActionWP3DS,
     frameAncestors: CSP_SELF,
@@ -152,6 +145,5 @@ module.exports = {
   rateLimitMiddleware,
   captureEventMiddleware,
   requestParseMiddleware,
-  detectErrorsMiddleware,
-  setResponseCspReportingEndpointName
+  detectErrorsMiddleware
 }
