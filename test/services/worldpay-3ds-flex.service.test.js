9192d5fe8ceed2ec0b76c0028e5c35c299655cfe
John Shields <john.shields@digital.cabinet-office.gov.uk>
2024-04-11 19:38:37 +0100

    PP-11682 Change sinon stub to return axios named values i.e. ‘data‘ and ‘status’.

--- test/services/worldpay-3ds-flex.service.test.js
+++ test/services/worldpay-3ds-flex.service.test.js
@@ -24,8 +24,8 @@ describe('Worldpay 3DS Flex service', () => {
       const jwtResponse = validDdcJwt(TEST_JWT)
       getWorldpay3dsFlexJwtStub = sinon.stub().resolves(
         {
-          statusCode: 200,
-          body: jwtResponse
+          status: 200,
+          data: jwtResponse
         })
       connectorClientStub = sinon.stub().callsFake(() => {
         return {
