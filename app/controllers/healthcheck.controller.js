c9154993f3a725e3350cce47dddfcb1e23f63e4a
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2024-04-12 15:49:19 +0100

    PP-11683 Update healthcheck controller to use Axios
    - Uses the Axios base client from `pay-js-commons` for the healthcheck
      controller.
    - Other clients will be replaced in with Axios base client in future
      PRs.

--- app/controllers/healthcheck.controller.js
+++ app/controllers/healthcheck.controller.js
@@ -2,7 +2,10 @@
 const _ = require('lodash')
 const logger = require('../utils/logger')(__filename)
 const { getLoggingFields } = require('../utils/logging-fields-helper')
-const baseClient = require('../services/clients/base.client/base.client')
+const { Client } = require('@govuk-pay/pay-js-commons/lib/utils/axios-base-client/axios-base-client')
+const { configureClient } = require('../services/clients/base/config')
+const SERVICE_NAME = 'frontend'
+const client = new Client(SERVICE_NAME)
 
 const healthyPingResponse = { ping: { healthy: true } }
 
@@ -11,21 +14,34 @@ const respond = (res, statusCode, data) => {
   res.end(JSON.stringify(data))
 }
 
-module.exports.healthcheck = (req, res) => {
+module.exports.healthcheck = async (req, res) => {
   if (process.env.FORWARD_PROXY_URL) {
-    baseClient.get(`${process.env.FORWARD_PROXY_URL}/nginx_status`, {}, (err, response) => {
-      const statusCode = _.get(response, 'statusCode')
-      if (err || statusCode !== 200) {
+    const url = `${process.env.FORWARD_PROXY_URL}/nginx_status`
+
+    configureClient(client, url)
+
+    let response
+
+    try {
+      response = await client.get(url, 'Healthcheck')
+
+      if (response.status !== 200) {
         logger.error('Healthchecking forward proxy returned error', {
           ...getLoggingFields(req),
-          error: err,
-          status_code: statusCode
+          status_code: response.status
         })
         respond(res, 502, _.merge(healthyPingResponse, { proxy: { healthy: false } }))
       } else {
-        respond(res, 200, _.merge(healthyPingResponse, { proxy: { healthy: true } }))
+        respond(res, 200, healthyPingResponse)
       }
-    })
+    } catch (err) {
+      logger.error('Healthchecking forward proxy returned error', {
+        ...getLoggingFields(req),
+        error: err,
+        status_code: response.status
+      })
+      respond(res, 502, _.merge(healthyPingResponse, { proxy: { healthy: false } }))
+    }
   } else {
     respond(res, 200, healthyPingResponse)
   }
