51c9b20f3bb19e82365d513b8536cce1c8ce0184
Marco Tranchino <marco.tranchino@digital.cabinet-office.gov.uk>
2024-05-30 11:34:50 +0100

    Throw CLIENT_UNAVAILABLE unless Connector returns success

--- app/models/token-axios.js
+++ app/models/token-axios.js
@@ -3,10 +3,15 @@
 // Local dependencies
 const connectorClient = require('../services/clients/connector-axios.client')
 
+const SUCCESS_CODES = [200, 201, 202, 204, 206]
+
 const markTokenAsUsed = async function (tokenId, loggingFields = {}) {
   let response
   try {
     response = await connectorClient().markTokenAsUsed({ tokenId }, loggingFields)
+    if (!SUCCESS_CODES.includes(response.status)) {
+      throw new Error('CLIENT_UNAVAILABLE')
+    }
   } catch (err) {
     throw new Error('CLIENT_UNAVAILABLE', err)
   }
