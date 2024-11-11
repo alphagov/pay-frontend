b9b0e64bef8668f4e5ab8df890cbd5ac0f64975a
Natalie.Carey <natalie.carey@digital.cabinet-office.gov.uk>
2024-11-11 16:44:21 +0000

    PP-13236: Added debugger integration.

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 module.exports = (on, config) => {
   const stubSetupUrl = config.env.MOCK_HTTP_SERVER_URL + '/__add-mock-endpoints__'
   const stubResetUrl = config.env.MOCK_HTTP_SERVER_URL + '/__clear-mock-endpoints__'
+  const createSnapshotUrl = config.env.MOCK_HTTP_SERVER_URL + '/__create-debug-snapshot__'
 
   // common task definitions - used by all test specs
   on('task', {
@@ -35,6 +36,16 @@ module.exports = (on, config) => {
         .catch(function (error) {
           throw error
         })
+    },
+    /**
+     * Makes a request to @govuk-pay to create a debug snapshot with whatever name is specified.
+     */
+    createDebugSnapshot (name) {
+      return axios.post(createSnapshotUrl, {
+        name
+      })
+        .then(function () { return '' })
+        .catch(function (error) { throw error })
     }
   })
   return config
