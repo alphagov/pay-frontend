1f562be258b0dc4bc3d5511dfbba68bdd4277b96
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2025-06-23 09:04:04 +0100

    PP-14256 - Update rebrand flag to set Nunjucks global
    - The rebrand flag from the middleware does not propogate
      to error pages.
      - This means the error pages are seeing the current
        branding when ENABLE_REBRAND = true.
    - Updated the rebrand flag to set a Nunjucks global instead.
    - Update tests to include testing an error page to ensure
      it has the right branding depending on the ENABLE_REBRAND
      flag.

--- app/middleware/add-rebrand-flag.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports = function (req, res, next) {
-  res.locals.enableRebrand = (process.env.ENABLE_REBRAND === 'true')
-  next()
-}
--- test/cypress/integration/card/token-403.test.cy.rebrand.js
+++ test/cypress/integration/card/token-403.test.cy.rebrand.js
@@ -1,10 +1,9 @@
 const cardPaymentStubs = require('../../utils/card-payment-stubs')
 
-describe('Re-using token results in 403 flow', () => {
+describe('Rebranding for error pages', () => {
   const tokenId = 'be88a908-3b99-4254-9807-c855d53f6b2b'
   const chargeId = 'ub8de8r5mh4pb49rgm1ismaqfv'
   const usedTokenAndReturnPaymentCreatedChargeStubs = cardPaymentStubs.buildUsedTokenAndReturnPaymentChargeStubs(tokenId, chargeId, 'ENTERING CARD DETAILS')
-  const returnNotFound = cardPaymentStubs.buildChargeFromTokenNotFound(tokenId)
 
   beforeEach(() => {
     cy.clearCookies()
@@ -16,30 +15,20 @@ describe('Re-using token results in 403 flow', () => {
       cy.visit(`/secure/${tokenId}`, { failOnStatusCode: false })
       cy.location('pathname').should('eq', `/secure/${tokenId}`)
 
-      cy.log('Should display the GOV.UK header correctly')
+      cy.log('Should display the header with new branding')
 
-      cy.get('[data-cy=header]').should('have.css', 'background-color', 'rgb(11, 12, 12)')
+      cy.get('[data-cy=header]').should('have.css', 'background-color', 'rgb(29, 112, 184)')
       cy.get('[data-cy=header]').should('have.css', 'color', 'rgb(255, 255, 255)')
       cy.get('[data-cy=header]')
         .find('.govuk-header__container')
-        .should('have.css', 'border-bottom-color', 'rgb(29, 112, 184)')
+        .should('have.css', 'border-bottom-color', 'rgb(255, 255, 255)')
 
-      cy.log('Should display the GOV.UK footer correctly')
+      cy.log('Should display the footer with new branding')
 
       cy.get('[data-cy=footer]')
-        .should('have.css', 'background-color', 'rgb(243, 242, 241)')
+        .should('have.css', 'background-color', 'rgb(244, 248, 251)')
         .should('have.css', 'border-top-color', 'rgb(29, 112, 184)')
 
-      cy.get('h1').should('contain', 'Your payment session has expired')
-      cy.get('#return-url').should('not.exist')
-    })
-  })
-
-  describe('Visiting the secure url when token doesn\'t exist', () => {
-    it('should show the "Your payment session has expired" page', () => {
-      cy.task('setupStubs', returnNotFound)
-      cy.visit(`/secure/${tokenId}`, { failOnStatusCode: false })
-      cy.location('pathname').should('eq', `/secure/${tokenId}`)
       cy.get('h1').should('contain', 'Your payment session has expired')
       cy.get('#return-url').should('not.exist')
     })
