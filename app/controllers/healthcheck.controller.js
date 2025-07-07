4adf712b846cc15baf3d2f62f68b20a28df3f9da
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2025-07-07 21:45:50 +0100

    PP-14027 Remove forward proxy code
    - Removed usages of FORWARD_PROXY_URL usage from healthcheck and also related code as clients doesn't use forward proxy anymore.

--- app/controllers/healthcheck.controller.js
+++ app/controllers/healthcheck.controller.js
@@ -1,13 +1,3 @@
-// NPM dependencies
-const _ = require('lodash')
-const logger = require('../utils/logger')(__filename)
-const { getLoggingFields } = require('../utils/logging-fields-helper')
-const { Client } = require('@govuk-pay/pay-js-commons/lib/utils/axios-base-client/axios-base-client')
-const { configureClient } = require('../services/clients/base/config')
-const SERVICE_NAME = 'frontend'
-const client = new Client(SERVICE_NAME)
-const { CORRELATION_HEADER } = require('../../config/correlation-header')
-
 const healthyPingResponse = { ping: { healthy: true } }
 
 const respond = (res, statusCode, data) => {
@@ -16,34 +6,5 @@ const respond = (res, statusCode, data) => {
 }
 
 module.exports.healthcheck = async (req, res) => {
-  if (process.env.FORWARD_PROXY_URL) {
-    const url = `${process.env.FORWARD_PROXY_URL}/nginx_status`
-
-    configureClient(client, url, req.headers[CORRELATION_HEADER])
-
-    let response
-
-    try {
-      response = await client.get(url, 'Healthcheck')
-
-      if (response.status !== 200) {
-        logger.error('Healthchecking forward proxy returned error', {
-          ...getLoggingFields(req),
-          status_code: response.status
-        })
-        respond(res, 502, _.merge(healthyPingResponse, { proxy: { healthy: false } }))
-      } else {
-        respond(res, 200, healthyPingResponse)
-      }
-    } catch (err) {
-      logger.error('Healthchecking forward proxy returned error', {
-        ...getLoggingFields(req),
-        error: err,
-        status_code: response.status
-      })
-      respond(res, 502, _.merge(healthyPingResponse, { proxy: { healthy: false } }))
-    }
-  } else {
-    respond(res, 200, healthyPingResponse)
-  }
+  respond(res, 200, healthyPingResponse)
 }
