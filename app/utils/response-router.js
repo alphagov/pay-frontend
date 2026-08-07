0f14c7ee91d1caa7fc78f0ca4f3465f45e42f3b6
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2026-08-07 12:17:06 +0100

    PP-15705 Salute to Rich Blake (#4096)
    Edit humans.txt to recognise the contributions of Rich Blake to Pay
    This will stay until Monday 10th August, 2026

--- app/utils/response-router.js
+++ app/utils/response-router.js
@@ -117,7 +117,7 @@ const actions = {
     code: 200,
     view: 'plain-message',
     locals: {
-      message: 'GOV.UK Pay is built by a team at the Government Digital Service in London, Manchester and Bristol. If you\'d like to join us, see https://gds.blog.gov.uk/jobs'
+      message: 'Thanks for everything, Rich.'
     }
   },
 
